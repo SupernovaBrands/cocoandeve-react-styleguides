c895701ef91df4829cbfe3f0074dc8d52c158e30
Teddy Maulana <maulanateddy99@gmail.com>
2023-01-16 12:18:24 +0700

    add react bootstrap

--- pages/components/buttons.jsx
+++ pages/components/buttons.jsx
@@ -1,10 +1,11 @@
-import Button from '../../src/components/button';
+// import Button from '../../src/components/button';
+import Button from 'react-bootstrap/Button';
 
 export default function Buttons() {
 	return (
 		<div>
 			<h1>Buttons</h1>
-			<Button class="btn btn-primary mb-2">Button</Button>
+			<Button>Button 2</Button>
 		</div>
 	);
 }
