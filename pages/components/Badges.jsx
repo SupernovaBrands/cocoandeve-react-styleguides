1910a9ccf78fdeb48135ce196ce9f9a6a1f7007c
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2023-01-17 14:29:14 +0700

    badge component

--- pages/components/Badges.jsx
+++ pages/components/Badges.jsx
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import Badges from 'react-bootstrap/Badge';
 
 export default function Buttons() {
@@ -6,9 +6,18 @@ export default function Buttons() {
 		<Container className="mt-4">
 			<h1>Badges</h1>
 			<div className="d-flex flex-column align-items-start">
-				<Badges bg="secondary">Tan</Badges>
+				<Badges bg="sh-purple" className="mb-1 mt-1">Tan</Badges>
 				<Badges bg="secondary" className="mb-1 mt-1">Hair</Badges>
+				<Badges bg="bali-bod-blue" className="mb-1 mt-1">Body</Badges>
 			</div>
+			<p className="my-4">Circle Badge with svg</p>
+			<Row>
+				<Col xs={9} lg={3} className="pt-4">
+					<picture class="circle-badge position-absolute rounded-circle bg-primary d-flex p-1">
+
+					</picture>
+				</Col>
+			</Row>
 		</Container>
 	)
 }
