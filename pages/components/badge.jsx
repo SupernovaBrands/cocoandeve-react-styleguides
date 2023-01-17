d4ea9c9120ad581f9d7020f4c3c7545e19ab0ba8
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2023-01-17 14:40:29 +0700

    badge component

--- pages/components/badge.jsx
+++ pages/components/badge.jsx
@@ -1,7 +1,7 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import Badges from 'react-bootstrap/Badge';
 
-export default function Buttons() {
+export default function Badge() {
 	return (
 		<Container className="mt-4">
 			<h1>Badges</h1>
@@ -12,9 +12,9 @@ export default function Buttons() {
 			</div>
 			<p className="my-4">Circle Badge with svg</p>
 			<Row>
-				<Col xs={9} lg={3} className="pt-4">
+				<Col xs={9} lg={3} className="pt-4 position-relative">
 					<picture class="circle-badge position-absolute rounded-circle bg-primary d-flex p-1">
-
+						<img alt="25% Off" className="w-100" src="/badge-25.svg" />
 					</picture>
 				</Col>
 			</Row>
