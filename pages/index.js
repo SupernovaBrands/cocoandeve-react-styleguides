4f7a9398d57c183b543eaa10735e8da1d8e41b58
pebiantara <pebiantara@gmail.com>
2023-01-26 17:26:11 +0700

    add index list files

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,14 @@ export default function Home() {
 
 	return (
 		<div className="container">
-			<div className="row">
-				<h1>Coco&Eve Styleguide</h1>
-
+			<h1 className="py-2">Coco&Eve Styleguide</h1>
+			<div className="row mt-3">
+				{ files && Object.keys(files).map((file) => (
+					<div className="col-12 col-lg-6 mb-3 d-flex flex-column align-items-start">
+						<h2>{file}</h2>
+						{ (files[file].map((subfile, index) => (<a key={`${subfile.title}-${index}`} href={subfile.link}>{subfile.title}</a>))) }
+					</div>
+				))}
 			</div>
 		</div>
 	);
