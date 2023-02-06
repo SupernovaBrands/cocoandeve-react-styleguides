b92a046e0eb60926160eba436ec01acb7bc7b4d1
pebiantara <pebiantara@gmail.com>
2023-02-06 15:21:52 +0700

    update config base path for github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isGithubActions = process.env.GITHUB_ACTIONS || false
-let assetPrefix = '_'
-let basePath = '/'
+let assetPrefix = ''
+let basePath = '/cocoandeve-react-styleguides'
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
