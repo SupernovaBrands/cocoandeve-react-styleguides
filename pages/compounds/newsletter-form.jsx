608b0c2949d36a332be4aaac31889c97a9060b11
Awais Gill <awais@supernovabrands.com>
2023-02-08 13:21:06 +0500

    Task/860ptyvby Button Fixes

--- pages/compounds/newsletter-form.jsx
+++ pages/compounds/newsletter-form.jsx
@@ -9,7 +9,7 @@ const NewsletterForm = () => {
                     <form>
                         <div className="input-group mb-3">
                             <input type="text" className="form-control" placeholder="Enter your email" aria-label="Enter your email" />
-                            <div className="input-group-append">
+                            <div className="input-group-append d-flex">
                                 <button className="btn btn-primary" type="button">Subscribe</button>
                             </div>
                         </div>
--- src/components/product-waitlist-oos.jsx
+++ src/components/product-waitlist-oos.jsx
@@ -7,7 +7,7 @@ const ProductWaitlist = (props) => {
             <p className="text-center font-size-sm px-lg-3 mb-1">Our products have been a worldwide hit and we can't keep up with the stock. But we are on it, sign up and we will notify you when it’s available!</p>
             <div className="input-group rounded">
                 <input type="email" name="email" className="form-control text-body border-right-0 rounded-right-0 rounded bg-white" placeholder="Enter your email" />
-                <button className="btn btn-primary rounded  mt-1 mt-lg-0 py-1 col-12 col-lg-auto" type="submit">Join the waitlist</button>
+                <button className="btn btn-primary rounded  mt-lg-0 py-1 col-12 col-lg-auto" type="submit">Join the waitlist</button>
             </div>
         </form>
 	);
