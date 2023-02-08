b0b00fc2667db01a90737ae9dfcac3486f7d9980
Teddy Maulana <maulanateddy99@gmail.com>
2023-02-08 17:18:26 +0700

    general updates

--- pages/components/svgs.jsx
+++ pages/components/svgs.jsx
@@ -1,8 +1,10 @@
 import { Container } from "react-bootstrap";
 import Account from '../../src/images/icons/account.svg';
+import AccountAbtest from '../../src/images/icons/account-abtest.svg';
 import AlcoholFree from '../../src/images/icons/alcohol-free.svg';
 import BackToTop from '../../src/images/icons/back-to-top.svg';
 import Cart from '../../src/images/icons/cart.svg';
+import CartAbtest from '../../src/images/icons/cart-abtest.svg';
 import CheckCircle from '../../src/images/icons/check-circle.svg';
 import Check from '../../src/images/icons/check.svg';
 import ChevronDown from '../../src/images/icons/chevron-down.svg';
@@ -33,6 +35,44 @@ import Minus from '../../src/images/icons/minus.svg';
 import MoneybackEur from '../../src/images/icons/moneyback-eur.svg';
 import MoneybackPounds from '../../src/images/icons/moneyback-pounds.svg';
 import Moneyback from '../../src/images/icons/moneyback.svg';
+import NaturalDha from '../../src/images/icons/natural-dha.svg';
+import PalmTree from '../../src/images/icons/palm-tree.svg';
+import ParabelFree from '../../src/images/icons/paraben-free.svg';
+import Paste from '../../src/images/icons/paste.svg';
+import Peta from '../../src/images/icons/peta.svg';
+import PinterestSquare from '../../src/images/icons/pinterest-square.svg';
+import Play from '../../src/images/icons/play.svg';
+import PlusCircle from '../../src/images/icons/plus-circle.svg';
+import Plus from '../../src/images/icons/plus.svg';
+import QuoteDown from '../../src/images/icons/quote-down.svg';
+import QuoteUp from '../../src/images/icons/quote-up.svg';
+import Recurring from '../../src/images/icons/recurring.svg';
+import Recycle from '../../src/images/icons/recycle.svg';
+import SearchAbtest from '../../src/images/icons/search-abtest.svg';
+import ShadeNew from '../../src/images/icons/shade-new.svg';
+import SmallPalmTree from '../../src/images/icons/small-palm-tree.svg';
+import StarBox from '../../src/images/icons/star-box.svg';
+import StarFull from '../../src/images/icons/star-full.svg';
+import StarHalf from '../../src/images/icons/star-half.svg';
+import StarLine from '../../src/images/icons/star-line.svg';
+import Star from '../../src/images/icons/star.svg';
+import SwellStarsCart from '../../src/images/icons/swell-stars-cart-sm.svg';
+import SwellStarsSm from '../../src/images/icons/swell-stars-sm.svg';
+import SwellStars from '../../src/images/icons/swell-stars.svg';
+import Tag from '../../src/images/icons/tag.svg';
+import ThumbsDown from '../../src/images/icons/thumbs-down.svg';
+import ThumbsUp from '../../src/images/icons/thumbs-up.svg';
+import Tiktok from '../../src/images/icons/tiktok.svg';
+import ToxinFree from '../../src/images/icons/toxin-free.svg';
+import Translate from '../../src/images/icons/translate.svg';
+import Trash from '../../src/images/icons/trash.svg';
+import TwitterSquare from '../../src/images/icons/twitter-square.svg';
+import TwoLineStars from '../../src/images/icons/two-line-stars.svg';
+import Vegan from '../../src/images/icons/vegan.svg'; 
+import Verified from '../../src/images/icons/verified.svg';   
+import WinnerAward from '../../src/images/icons/winner-award.svg';
+import Youtube from '../../src/images/icons/youtube.svg'; 
+
  
 const Svgs = (props) => {
 
@@ -44,6 +84,12 @@ const Svgs = (props) => {
                         <Account className="svg" />
                     </span>
                     <span className='label'>Account</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <AccountAbtest className="svg" />
+                    </span>
+                    <span className='label'>AccountAbtest</span>
                 </div>
                 <div className="preview d-flex flex-column text-center">
                     <span className="inner d-flex justify-content-center align-items-center">
@@ -62,6 +108,12 @@ const Svgs = (props) => {
                         <Cart className="svg" />
                     </span>
                     <span className='label'>Cart</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <CartAbtest className="svg" />
+                    </span>
+                    <span className='label'>CartAbtest</span>
                 </div>
                 <div className="preview d-flex flex-column text-center">
                     <span className="inner d-flex justify-content-center align-items-center">
@@ -254,6 +306,228 @@ const Svgs = (props) => {
                         <Moneyback className="svg" />
                     </span>
                     <span className='label'>Moneyback</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <NaturalDha className="svg" />
+                    </span>
+                    <span className='label'>NaturalDha</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <PalmTree className="svg" />
+                    </span>
+                    <span className='label'>PalmTree</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <ParabelFree className="svg" />
+                    </span>
+                    <span className='label'>ParabelFree</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Paste className="svg" />
+                    </span>
+                    <span className='label'>Paste</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Peta className="svg" />
+                    </span>
+                    <span className='label'>Peta</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <PinterestSquare className="svg" />
+                    </span>
+                    <span className='label'>PinterestSquare</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Play className="svg" />
+                    </span>
+                    <span className='label'>Play</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <PlusCircle className="svg" />
+                    </span>
+                    <span className='label'>PlusCircle</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Plus className="svg" />
+                    </span>
+                    <span className='label'>Plus</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <QuoteDown className="svg" />
+                    </span>
+                    <span className='label'>QuoteDown</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <QuoteUp className="svg" />
+                    </span>
+                    <span className='label'>QuoteUp</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Recurring className="svg" />
+                    </span>
+                    <span className='label'>Recurring</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Recycle className="svg" />
+                    </span>
+                    <span className='label'>Recycle</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <SearchAbtest className="svg" />
+                    </span>
+                    <span className='label'>SearchAbtest</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <ShadeNew className="svg" />
+                    </span>
+                    <span className='label'>ShadeNew</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <SmallPalmTree className="svg" />
+                    </span>
+                    <span className='label'>SmallPalmTree</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <StarBox className="svg" />
+                    </span>
+                    <span className='label'>StarBox</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <StarFull className="svg" />
+                    </span>
+                    <span className='label'>StarFull</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <StarHalf className="svg" />
+                    </span>
+                    <span className='label'>StarHalf</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <StarLine className="svg" />
+                    </span>
+                    <span className='label'>StarLine</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Star className="svg" />
+                    </span>
+                    <span className='label'>Star</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <SwellStarsCart className="svg" />
+                    </span>
+                    <span className='label'>SwellStarsCart</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <SwellStarsSm className="svg" />
+                    </span>
+                    <span className='label'>SwellStarsSm</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <SwellStars className="svg" />
+                    </span>
+                    <span className='label'>SwellStars</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Tag className="svg" />
+                    </span>
+                    <span className='label'>Tag</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <ThumbsDown className="svg" />
+                    </span>
+                    <span className='label'>ThumbsDown</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <ThumbsUp className="svg" />
+                    </span>
+                    <span className='label'>ThumbsUp</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Tiktok className="svg" />
+                    </span>
+                    <span className='label'>Tiktok</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <ToxinFree className="svg" />
+                    </span>
+                    <span className='label'>ToxinFree</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Translate className="svg" />
+                    </span>
+                    <span className='label'>Translate</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Trash className="svg" />
+                    </span>
+                    <span className='label'>Trash</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <TwitterSquare className="svg" />
+                    </span>
+                    <span className='label'>TwitterSquare</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <TwoLineStars className="svg" />
+                    </span>
+                    <span className='label'>TwoLineStars</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Vegan className="svg" />
+                    </span>
+                    <span className='label'>Vegan</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Verified className="svg" />
+                    </span>
+                    <span className='label'>Verified</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <WinnerAward className="svg" />
+                    </span>
+                    <span className='label'>WinnerAward</span>
+                </div>
+                <div className="preview d-flex flex-column text-center">
+                    <span className="inner d-flex justify-content-center align-items-center">
+                        <Youtube className="svg" />
+                    </span>
+                    <span className='label'>Youtube</span>
                 </div>
             </div>
         </Container>
