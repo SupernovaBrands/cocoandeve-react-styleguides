b3a5ec2322c3518ec44220649b5ea1f3605d17b8
Awais Gill <awais@supernovabrands.com>
2023-02-17 09:12:08 +0500

    Task/860ptyvby

--- src/compounds/product-variant.jsx
+++ src/compounds/product-variant.jsx
@@ -1,11 +1,19 @@
+import Help from '../../src/images/icons/help.svg';
+
 const ProductVariant = (props) => {
 	return (
         <div className="product-variant custom-radio w-100 mb-1">
             <input id={props.id} className="custom-control-input" type="radio" name={props.name} value={props.value} data-inventory={props.inventory} defaultChecked={props.checked} data-id={props.dataID} />
             <label htmlFor={props.id} className="custom-control-label w-100 border py-2 pr-2 rounded mb-1">
-                <p className="mb-1 fw-bold">
+                <p className="mb-1 fw-bold d-flex">
                     {props.title}
+                    {props.subscription &&
+                        <a className="text-primary ms-1" data-container="body" data-toggle="popover" data-placement="top" data-content="<strong>No fuss and frizz, just good hair days ahead.</strong><br/>Keep your hair routine in check and never run out of your washday must-haves every month and free shipping on top of that.<br/><br/>Cancel anytime, and come back when you're ready. ❤️" data-html="true">
+                            <Help className="svg" />
+                        </a>
+                    }
                 </p>
+                
                 {props.title_subscription &&
                     <p className="mb-1 fw-bold">{props.title_subscription}</p>
                 }
