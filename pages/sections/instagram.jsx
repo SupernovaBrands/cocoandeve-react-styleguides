bac563748171954d85b25aa069eff2985d1dc7e0
Teddy Maulana <maulanateddy99@gmail.com>
2023-03-23 11:32:05 +0700

    instagram sections

---
+++ pages/sections/instagram.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import Instagram from '@/sections/Instagram';
+
+const InstagramSection = () => {
+    return (
+        <Instagram />
+    );
+}
+
+export default InstagramSection;
