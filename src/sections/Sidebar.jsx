28b51ef67b685e7d565588007fca78cbea5ac274
Awais Gill <awais@supernovabrands.com>
2023-04-12 23:32:40 +0500

    [Sidebar] Update as base component

--- src/sections/Sidebar.jsx
+++ src/sections/Sidebar.jsx
@@ -14,33 +14,35 @@ const Sidebar = (props) => {
 	return (
 		<Col md={12} lg={4}>
 			<aside className="blog-grid__sidebar sidebar position-lg-sticky col-12 d-lg-block mb-4 mb-lg-0 align-self-start">
-				<h4 className="mb-3 h1">POPULAR READS</h4>
-				<div className="position-relative">
-					{arrayN.length > 0 && (
-						<>
-							<ol className="carousel-indicators carousel-indicators--primary mb-0 d-lg-none">
-								{arrayN.map((item, i) => (
-									<li key={i} onClick={carouselHandle} data-bs-target=".carousel--sidebar" data-bs-slide-to={item} className={`rounded-circle ${index === i ? 'active' : ''}`}></li>
-								))}
-							</ol>
-							<Carousel activeIndex={index} className="carousel--sidebar" interval={null} controls={false} indicators={false}>
-								{arrayN.map((item, i) => {
-									const idx = item * 2;
-									const sliced = props.data.slice(idx, (item + 1) * 2);
-									return (
-										<Carousel.Item key={i}>
-											{sliced.map((data) => (
-												<PostCard key={data.id} className="mb-2 mb-lg-3">
-													<h3><Link href={data.link} className="link-secondary">{data.title}</Link></h3>
-													<p className="mt-1 mb-0">{data.desc}</p>
-												</PostCard>
-											))}
-										</Carousel.Item>
-									);
-								})}
-							</Carousel>
-						</>
-					)}
+				<div className="no-gutters__in-container">
+					<h4 className="mb-3 h1">POPULAR READS</h4>
+					<div className="position-relative">
+						{arrayN.length > 0 && (
+							<>
+								<ol className="carousel-indicators carousel-indicators--primary mb-0 d-lg-none">
+									{arrayN.map((item, i) => (
+										<li key={i} onClick={carouselHandle} data-bs-target=".carousel--sidebar" data-bs-slide-to={item} className={`rounded-circle ${index === i ? 'active' : ''}`}></li>
+									))}
+								</ol>
+								<Carousel activeIndex={index} className="carousel--sidebar" interval={null} controls={false} indicators={false}>
+									{arrayN.map((item, i) => {
+										const idx = item * 2;
+										const sliced = props.data.slice(idx, (item + 1) * 2);
+										return (
+											<Carousel.Item key={i}>
+												{sliced.map((data) => (
+													<PostCard key={data.id} className="mb-2 mb-lg-3">
+														<h3><Link href={data.link} className="link-secondary">{data.title}</Link></h3>
+														<p className="mt-1 mb-0">{data.desc}</p>
+													</PostCard>
+												))}
+											</Carousel.Item>
+										);
+									})}
+								</Carousel>
+							</>
+						)}
+					</div>
 				</div>
 			</aside>
 		</Col>
