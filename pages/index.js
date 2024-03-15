61c5cac761698e350c34db558247336333fed856
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2024-03-15 16:42:25 +0700

    86env4r3y tw migrate

--- pages/index.js
+++ pages/index.js
@@ -3,82 +3,82 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container mx-auto px-g">
       <h1 className="py-2">Coco&Eve Styleguide</h1>
-      <div className="row mt-3">
-        <div className="col-12 col-lg-6 mb-3 d-flex flex-column align-items-start">
+      <div className="lg:-mx-g flex flex-wrap mt-3">
+        <div className="w-full lg:w-1/2 px-0 lg:px-g mb-3 flex flex-col items-start">
           <h2>components</h2>
-          <a href="./components/accordions">Accordions</a>
-          <a href="./components/announcement-bar">Announcement Bar</a>
-          <a href="./components/badge">Badge</a>
-          <a href="./components/breadcrumbs">Breadcrumbs</a>
-          <a href="./components/bullets">Bullets</a>
-          <a href="./components/buttons">Buttons</a>
-          <a href="./components/carousels">Carousels</a>
-          <a href="./components/ce-logo">Ce Logo</a>
-          <a href="./components/colors">Colors</a>
-          <a href="./components/cookie-banners">Cookie Banners</a>
-          <a href="./components/dropdowns">Dropdowns</a>
-          <a href="./components/forms">Forms</a>
-          <a href="./components/grid">Grid</a>
-          <a href="./components/hr">Hr</a>
-          <a href="./components/product-image-carousels">
+          <Link href="./components/accordions">Accordions</Link>
+          <Link href="./components/announcement-bar">Announcement Bar</Link>
+          <Link href="./components/badge">Badge</Link>
+          <Link href="./components/breadcrumbs">Breadcrumbs</Link>
+          <Link href="./components/bullets">Bullets</Link>
+          <Link href="./components/buttons">Buttons</Link>
+          <Link href="./components/carousels">Carousels</Link>
+          <Link href="./components/ce-logo">Ce Logo</Link>
+          <Link href="./components/colors">Colors</Link>
+          <Link href="./components/cookie-banners">Cookie Banners</Link>
+          <Link href="./components/dropdowns">Dropdowns</Link>
+          <Link href="./components/forms">Forms</Link>
+          <Link href="./components/grid">Grid</Link>
+          <Link href="./components/hr">Hr</Link>
+          <Link href="./components/product-image-carousels">
             Product Image Carousels
-          </a>
-          <a href="./components/svgs">Svgs</a>
-          <a href="./components/tabs">Tabs</a>
-          <a href="./components/tooltips">Tooltips</a>
-          <a href="./components/vide-cards">Vide Cards</a>
-          <a href="./components/video-loop-gif">Video Loop Gif</a>
+          </Link>
+          <Link href="./components/svgs">Svgs</Link>
+          <Link href="./components/tabs">Tabs</Link>
+          <Link href="./components/tooltips">Tooltips</Link>
+          <Link href="./components/vide-cards">Vide Cards</Link>
+          <Link href="./components/video-loop-gif">Video Loop Gif</Link>
         </div>
 
-        <div className="col-12 col-lg-6 mb-3 d-flex flex-column align-items-start">
+        <div className="w-full lg:w-1/2 px-0 lg:px-g mb-3 flex flex-col items-start">
           <h2>compounds</h2>
-          <a href="./compounds/image-with-texts">Image With Texts</a>
-          <a href="./compounds/launch-waitlist">Launch Waitlist</a>
-          <a href="./compounds/nav-category">Nav Category</a>
-          <a href="./compounds/newsletter-form">Newsletter Form</a>
-          <a href="./compounds/popups">Popups</a>
-          <a href="./compounds/post-cards">Post Cards</a>
-          <a href="./compounds/product-banner">Product Banner</a>
-          <a href="./compounds/product-card-abtest">Product Card Abtest</a>
-          <a href="./compounds/product-card">Product Card</a>
-          <a href="./compounds/product-variant">Product Variant</a>
-          <a href="./compounds/result-card">Result Card</a>
-          <a href="./compounds/upsell">Upsell</a>
-          <a href="./compounds/waitlist-form">Waitlist Form</a>
+          <Link href="./compounds/image-with-texts">Image With Texts</Link>
+          <Link href="./compounds/launch-waitlist">Launch Waitlist</Link>
+          <Link href="./compounds/nav-category">Nav Category</Link>
+          <Link href="./compounds/newsletter-form">Newsletter Form</Link>
+          <Link href="./compounds/popups">Popups</Link>
+          <Link href="./compounds/post-cards">Post Cards</Link>
+          <Link href="./compounds/product-banner">Product Banner</Link>
+          <Link href="./compounds/product-card-abtest">Product Card Abtest</Link>
+          <Link href="./compounds/product-card">Product Card</Link>
+          <Link href="./compounds/product-variant">Product Variant</Link>
+          <Link href="./compounds/result-card">Result Card</Link>
+          <Link href="./compounds/upsell">Upsell</Link>
+          <Link href="./compounds/waitlist-form">Waitlist Form</Link>
         </div>
-        <div className="col-12 col-lg-6 mb-3 d-flex flex-column align-items-start">
+        <div className="w-full lg:w-1/2 px-0 lg:px-g mb-3 flex flex-col items-start">
           <h2>Sections</h2>
-          <a href="./sections/cart-drawer">Cart Drawer</a>
-          <a href="./sections/comments">Comments</a>
-          <a href="./sections/footer">Footer</a>
-          <a href="./sections/header">Header</a>
-          <a href="./sections/hero-banner">Hero Banner</a>
-          <a href="./sections/howto">HowTo</a>
-          <a href="./sections/instagram">Instagram</a>
-          <a href="./sections/playground">Playground</a>
-          <a href="./sections/product-carousels">Product Carousel</a>
-          <a href="./sections/real-results">Real Results</a>
-          <a href="./sections/services">Services</a>
-          <a href="./sections/sidebars">Sidebar</a>
-          <a href="./sections/testimonials-carousel">Testimonials Carousel</a>
-          <a href="./sections/testimonials">Testimonials</a>
+          <Link href="./sections/cart-drawer">Cart Drawer</Link>
+          <Link href="./sections/comments">Comments</Link>
+          <Link href="./sections/footer">Footer</Link>
+          <Link href="./sections/header">Header</Link>
+          <Link href="./sections/hero-banner">Hero Banner</Link>
+          <Link href="./sections/howto">HowTo</Link>
+          <Link href="./sections/instagram">Instagram</Link>
+          <Link href="./sections/playground">Playground</Link>
+          <Link href="./sections/product-carousels">Product Carousel</Link>
+          <Link href="./sections/real-results">Real Results</Link>
+          <Link href="./sections/services">Services</Link>
+          <Link href="./sections/sidebars">Sidebar</Link>
+          <Link href="./sections/testimonials-carousel">Testimonials Carousel</Link>
+          <Link href="./sections/testimonials">Testimonials</Link>
 
         </div>
-        <div className="col-12 col-lg-6 mb-3 d-flex flex-column align-items-start">
+        <div className="w-full lg:w-1/2 px-0 lg:px-g mb-3 flex flex-col items-start">
           <h2>Templates</h2>
-          <a href="./templates/article">Article</a>
-          <a href="./templates/blog-filtered">Blog Filtered</a>
-          <a href="./templates/collection">Collection</a>
-          <a href="./templates/product">Product</a>
-          <a href="./templates/homepage">Homepage</a>
-          <a href="./templates/real-results">RealResults</a>
-          <a href="./templates/blog">Blog</a>
-          <a href="./templates/stockist">Stockist</a>
-          <a href="./templates/sustainability">Sustainability</a>
-          <a href="./templates/sweepstakes">Sweepstakes</a>
-          <a href="./templates/our-story">Our Story</a>
+          <Link href="./templates/article">Article</Link>
+          <Link href="./templates/blog-filtered">Blog Filtered</Link>
+          <Link href="./templates/collection">Collection</Link>
+          <Link href="./templates/product">Product</Link>
+          <Link href="./templates/homepage">Homepage</Link>
+          <Link href="./templates/real-results">RealResults</Link>
+          <Link href="./templates/blog">Blog</Link>
+          <Link href="./templates/stockist">Stockist</Link>
+          <Link href="./templates/sustainability">Sustainability</Link>
+          <Link href="./templates/sweepstakes">Sweepstakes</Link>
+          <Link href="./templates/our-story">Our Story</Link>
         </div>
       </div>
     </div>
---
+++ tailwind.config.js
@@ -0,0 +1,80 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '992px',
+      xl: '1260px',
+      xxl: '1440px',
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: '540px',
+        md: '720px',
+        lg: '960px',
+        xl: '1200px',
+        xxl: '1200px',
+      },
+    },
+    colors: {
+      'primary': '#D62E55',
+      'primary-darken': '#991e3b',
+      'primary-light': '#F5DADF',
+      'primary-light-second': '#FAE6EA',
+      'primary-dark': '#B22D4B',
+      'secondary': '#00635B',
+      'secondary-light': '#E0F0E9',
+      'sh-purple': '#993366',
+      'sh-purple-light': '#FFEFFF',
+      'bali-bod-blue': '#213E7B',
+      'bali-bod-blue-light': '#bec5d8',
+      'sh-orange': '#FF9900',
+      'sh-orange-light': '#FCF6E4',
+      'green-light': '#00B67A',
+      'yellow-light': '#f9eeca',
+      'pink-light': '#FFF2F4',
+      'pink-product': '#FFF4F6',
+      'suncare-blue': '#3880DE',
+      'skincare-orange': '#E85862',
+      'skin-peach': '#F2D4D7',
+      'body-color': '#151515',
+      'gray-100': '#FAF9F9',
+      'gray-400': '#F5F5F5',
+      'gray-500': '#A3A3A3',
+      'gray-600': '#4E4E4E',
+      'gray-700': '#7B7B7B',
+    },
+    fontFamily: {
+      sans: ['sofia-pro', 'sans-serif'],
+    },
+    fontSize: {
+      xs: ['0.75rem', '0.938rem'], //font size 12px, line height 15px
+      sm: ['0.875rem', '1.094rem'], //14px, 17.5px
+      base: ['1rem', '1.25rem'], // 16px, 20px
+      lg: ['1.25rem', '1.563rem'], //20px, 25px
+      xl: ['1.5rem', '1.875rem'], //24px, 30px
+      '2xl': ['2rem', '2.5rem'], //32px, 40px
+      '3xl': ['2.625rem', '3.281rem'], //42px, 52px
+    },
+    spacing: {
+      25: '5px',
+      1: '10px',
+      2: '20px',
+      3: '30px',
+      4: '40px',
+      5: '50px',
+      g: '15px',
+      hg: '7.5px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
