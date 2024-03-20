b432322115e64a7bd0eb12a28794de7d50aa722a
pebiantara <pebiantara@gmail.com>
2024-03-20 15:00:39 +0700

    add test badge circle img tsx

---
+++ src/components/BadgeCircleImg.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+
+interface BadgeCircleImageProps {
+  className?: string;
+  children?: ReactNode;
+}
+
+const BadgeCircleImage: React.FC<BadgeCircleImageProps> = (props) => {
+  return (
+    <picture className={`circle-badge position-absolute rounded-circle bg-primary d-flex p-1 ${props.className}`}>
+      {props.children}
+    </picture>
+  );
+};
+
+export default BadgeCircleImage;
