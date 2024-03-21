5f691047b656d06dac45ca2a32c9d87594c53e28
Awais Gill <awais@supernovabrands.com>
2024-03-21 17:20:41 +0500

    Buttons Component Tailwind Conversion

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,8 @@ module.exports = {
       'gray-500': '#A3A3A3',
       'gray-600': '#4E4E4E',
       'gray-700': '#7B7B7B',
+      'white': '#fff',
+      'transparent': 'transparent'
     },
     fontFamily: {
       sans: ['sofia-pro', 'sans-serif'],
