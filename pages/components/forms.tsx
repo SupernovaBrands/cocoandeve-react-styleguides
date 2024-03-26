ae9ba1eaa481d7367e1f7437cb545311359d4402
pebiantara <pebiantara@gmail.com>
2024-03-26 20:26:31 +0700

    update components form

--- pages/components/forms.tsx
+++ pages/components/forms.tsx
@@ -18,32 +18,32 @@ const Forms: React.FC = () => {
                 <div className="flex flew-wrap">
                     <div className="w-full">
                         <form>
-                            <div className="flex flew-wrap -mx-2">
+                            <div className="flex flew-wrap -mx-2 flex-col lg:flex-row">
                                 <InputFormGroup type="text" placeholder="Name" groupClass="w-full lg:w-1/2 pr-2 pl-2"></InputFormGroup>
                                 <InputFormGroup type="email" placeholder="Email*" groupClass="w-full lg:w-1/2 pr-2 pl-2"></InputFormGroup>
                             </div>
-                            <div className="flex flew-wrap -mx-2">
+                            <div className="flex flew-wrap -mx-2 flex-col lg:flex-row">
                                 <InputFormGroup type="text" placeholder="Name disabled" groupClass="w-full lg:w-1/2 pr-2 pl-2" disabled={true}></InputFormGroup>
                                 <InputFormGroup type="email" placeholder="Email disabled" groupClass="w-full lg:w-1/2 pr-2 pl-2" disabled={true}></InputFormGroup>
                             </div>
-                            <div className="flex flew-wrap -mx-2">
+                            <div className="flex flew-wrap -mx-2 flex-col lg:flex-row">
                                 <div className="w-full lg:w-1/2">
                                     <Select id="select-1" border={true} placeholder="Select Option" options={[{value: '1', label: 'Option 1'}, {value: '2', label: 'Option 2'}]} selected="2"></Select>
                                 </div>
-                                <div className="w-full lg:w-1/2">
+                                <div className="w-full lg:w-1/2 flex-col lg:flex-row">
                                     <Select id="select-1" border={true} placeholder="Select Option" options={[{value: '1', label: 'Option 1'}, {value: '2', label: 'Option 2'}]} selected="1" disabled={true}></Select>
                                 </div>
                             </div>
-                            <div className="flex flew-wrap">
+                            <div className="flex flew-wrap flex-col lg:flex-row">
                                 <div className="w-full">
                                     <TextArea id="text-area-1" rows={7} placeholder="Your Comment"></TextArea>
                                 </div>
                             </div>
-                            <div className="flex flew-wrap justify-center">
+                            <div className="flex flew-wrap lg:justify-center">
                                 <CheckBox label={`I agree to <a href=\"#\">Privacy Policy & ToS<\/a>`} id="agreement" checked={true}/>
                             </div>
                             <div className="flex flew-wrap">
-                                <div className="w-full">
+                                <div className="w-full mt-1">
                                     <Button label="Submit form" type="submit"></Button>
                                 </div>
                             </div>
