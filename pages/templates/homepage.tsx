dd3c5c9493b590270a1353d477ab1cc22d49952e
Teddy Maulana <maulanateddy99@gmail.com>
2024-04-01 10:21:03 +0700

    update homepage

--- pages/templates/homepage.tsx
+++ pages/templates/homepage.tsx
@@ -4,9 +4,7 @@ import Homepage from '@/templates/Homepage';
 import { useState } from 'react';
 // import Header from '@/sections/Header';
 import Footer from '@/sections/Footer';
-const Cart = dynamic(() => import('@/components/cart/cart'), {
-    ssr: false,
-});
+
 // import dynamic from 'next/dynamic';
 const Header = dynamic(() => import('@/sections/Header'), {
     ssr: false,
@@ -18,12 +16,12 @@ const HomepageTemplate = () => {
     const toggleCart = () => {
 		setShowCart(!showCart);
 	}
+
     return (
 		<>
 			<Header toggleCart={toggleCart}  />
         	<Homepage />
 			<Footer />
-			<Cart showCart={showCart} toggleCart={toggleCart} />
 		</>
     );
 }
