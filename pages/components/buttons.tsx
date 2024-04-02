47b927d28c0e0aa7a6f8cf87128f886d6cbf683d
Awais Gill <awais@supernovabrands.com>
2024-04-02 23:54:14 +0500

    task/86ep0xerv Buttons

--- pages/components/buttons.tsx
+++ pages/components/buttons.tsx
@@ -5,7 +5,7 @@ import Spin from '../../src/images/icons/spin.svg';
 export default function Buttons() {
 	return (
 		<div className="container my-4">
-			<h1>Buttons</h1>
+			<h1 className="mb-1">Buttons</h1>
 			<button className="bg-primary hover:bg-primary-darken rounded border border-transparent font-bold text-white py-[9px] px-[28px] mb-2">
 				Button
 			</button>
@@ -19,11 +19,11 @@ export default function Buttons() {
 			</button>
 			<br />
 			<button className="bg-primary hover:bg-primary-darken rounded border border-transparent font-bold text-white py-[9px] px-[28px] flex items-center">
-				Button With icon <span className='ml-[5px]'><Search className="h-[12px]" fill="white"/></span>
+				Button With Icon <span className="ml-[4px] mt-[5px]"><Search className="h-[12px]" fill="white"/></span>
 			</button>
 			<br />
 			<button className="bg-primary hover:bg-primary-darken rounded border border-transparent font-bold text-white py-[9px] px-[28px] flex items-center">
-				Shop <span className='ml-[5px]'><ChevronDown className="h-[12px]" fill="white"/></span>
+				Shop <span className="ml-[4px] mt-[5px]"><ChevronDown className="h-[12px]" fill="white"/></span>
 			</button>
 			<br />
 			<button className="bg-primary hover:bg-primary-darken rounded-lg border-2 border-primary font-bold text-white py-[13px] px-[54px] mb-2">
@@ -42,15 +42,15 @@ export default function Buttons() {
 				Button Block
 			</button>
 			<br />
-			<button className="bg-transparent hover:bg-primary hover:text-white w-full rounded border border-primary font-bold text-primary py-[9px] px-[28px] mb-2">
+			<button className="bg-transparent hover:bg-primary hover:text-white w-full rounded border border-primary font-bold text-primary py-[9px] px-[28px] mb-2 mt-[0.5rem]">
 				Button Block
 			</button>
 			<br />
-			<button className="bg-primary hover:bg-primary-darken w-full rounded-lg border border-transparent font-bold text-white py-[13px] px-[54px] mb-2">
+			<button className="bg-primary hover:bg-primary-darken w-full rounded-lg border border-transparent font-bold text-white py-[13px] px-[54px] mb-2 mt-[0.5rem]">
 				Button Large Block
 			</button>
 			<br />
-			<button className="bg-transparent hover:bg-primary hover:text-white w-full rounded-lg border-2 border-primary font-bold text-primary py-[13px] px-[54px] mb-2">
+			<button className="bg-transparent hover:bg-primary hover:text-white w-full rounded-lg border-2 border-primary font-bold text-primary py-[13px] px-[54px]">
 				Button Large Block
 			</button>
 		</div>
