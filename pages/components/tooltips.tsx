61defed3650ad7a20fdfc7751b945f8497726f2c
Teddy Maulana <maulanateddy99@gmail.com>
2024-04-02 12:05:24 +0700

    update spaces

--- pages/components/tooltips.tsx
+++ pages/components/tooltips.tsx
@@ -3,9 +3,9 @@ import Tooltip from '@/components/Tooltip';
 const Tooltips = () => {
 	return (
 		<>
-            <h1>Tooltips</h1>
+			<h1>Tooltips</h1>
 			<div className='container mx-auto sm:px-4 relative'>
-            	<Tooltip show={true} />
+				<Tooltip show={true} />
 			</div>
 		</>
 	);
--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -3,10 +3,10 @@ import CloseCircle from '@/images/icons/close-circle.svg';
 const Tooltip = (props: any) => {
 	return (
 		<div className={`absolute right-0 before:absolute before:top-0 before:right-0 w-full lg:w-1/3 pr-4 pl-4 tooltip bg-primary text-white text-center py-2 overflow-hidden ${props.show ? 'opacity-100 translate-y-[0] visible' : ''}`}>
-            <CloseCircle className="fill-[white] h-[1em] text-white cursor-pointer absolute right-[0.5em] top-2/4 -translate-y-1/2 text-[1.1875em]" />
-            Item added to cart
-            <a href="#" className="block text-white underline">Proceed to checkout</a>
-        </div>
+			<CloseCircle className="fill-[white] h-[1em] text-white cursor-pointer absolute right-[0.5em] top-2/4 -translate-y-1/2 text-[1.1875em]" />
+			Item added to cart
+			<a href="#" className="block text-white underline">Proceed to checkout</a>
+		</div>
 	);
 }
 
