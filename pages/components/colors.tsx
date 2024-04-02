6d42151d595ec7cfa285133fe46cbc2a4689e303
Awais Gill <awais@supernovabrands.com>
2024-04-03 00:00:25 +0500

    task/86ep0xet5 Colors

--- pages/components/colors.tsx
+++ pages/components/colors.tsx
@@ -1,191 +1,193 @@
 const Colors = (props) => {
 	return (
-		<div className="container mt-4">
-			<h1 className="mb-1">Colors</h1>
-            <h2 className="mb-1">Primary</h2>
-            <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-primary"></div>
-                    <div className="text-center">
-                        <p>Primary</p>
-                        <p>#D62E55</p>
+        <div className="mobile-wrapper sm:px-hg">
+            <div className="container mt-4">
+                <h1 className="mb-1">Colors</h1>
+                <h2 className="mb-1">Primary</h2>
+                <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-primary"></div>
+                        <div className="text-center">
+                            <p>Primary</p>
+                            <p>#D62E55</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-primary-light"></div>
+                        <div className="text-center">
+                            <p>Primary Light</p>
+                            <p>#F5DADF</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-primary-dark"></div>
+                        <div className="text-center">
+                            <p>Primary Dark</p>
+                            <p>#B22D4B</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-primary-light-second"></div>
+                        <div className="text-center">
+                            <p>Primary Light Second</p>
+                            <p>#FAE6EA</p>
+                        </div>
                     </div>
                 </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-primary-light"></div>
-                    <div className="text-center">
-                        <p>Primary Light</p>
-                        <p>#F5DADF</p>
+                <h2 className="mb-1">Secondary</h2>
+                <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-secondary"></div>
+                        <div className="text-center">
+                            <p>Secondary</p>
+                            <p>#00635B</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-secondary-light"></div>
+                        <div className="text-center">
+                            <p>Secondary Light</p>
+                            <p>#E0F0E9</p>
+                        </div>
                     </div>
                 </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-primary-dark"></div>
-                    <div className="text-center">
-                        <p>Primary Dark</p>
-                        <p>#B22D4B</p>
+                <h2 className="mb-1">Brand</h2>
+                <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-sh-purple"></div>
+                        <div className="text-center">
+                            <p>SH Purple</p>
+                            <p>#993366</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-sh-purple-light"></div>
+                        <div className="text-center">
+                            <p>SH Purple Light</p>
+                            <p>#FFEFFF</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-bali-bod-blue"></div>
+                        <div className="text-center">
+                            <p>Bali Bod Blue</p>
+                            <p>#213E7B</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-bali-bod-blue-light"></div>
+                        <div className="text-center">
+                            <p>Bali Bod Blue Light</p>
+                            <p>#bec5d8</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-sh-orange"></div>
+                        <div className="text-center">
+                            <p>SH Orange</p>
+                            <p>#FF9900</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-sh-orange-light"></div>
+                        <div className="text-center">
+                            <p>SH Orange Light</p>
+                            <p>#FCF6E4</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-green-light"></div>
+                        <div className="text-center">
+                            <p>Green Light</p>
+                            <p>#00B67A</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-yellow-light"></div>
+                        <div className="text-center">
+                            <p>Yellow Light</p>
+                            <p>#f9eeca</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-pink-light"></div>
+                        <div className="text-center">
+                            <p>Pink Light</p>
+                            <p>#FFF2F4</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-suncare-blue"></div>
+                        <div className="text-center">
+                            <p>Suncare Blue</p>
+                            <p>#3880DE</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-skincare-orange"></div>
+                        <div className="text-center">
+                            <p>Skincare Orange</p>
+                            <p>#E85862</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-pink-product"></div>
+                        <div className="text-center">
+                            <p>Pink Product</p>
+                            <p>#FFF4F6</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-skin-peach"></div>
+                        <div className="text-center">
+                            <p>Skin Peach</p>
+                            <p>#F2D4D7</p>
+                        </div>
                     </div>
                 </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-primary-light-second"></div>
-                    <div className="text-center">
-                        <p>Primary Light Second</p>
-                        <p>#FAE6EA</p>
+                <h2 className="mb-1">Body</h2>
+                <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-body-color"></div>
+                        <div className="text-center">
+                            <p>Body</p>
+                            <p>#151515</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-gray-600"></div>
+                        <div className="text-center">
+                            <p>Secondary Body</p>
+                            <p>#4E4E4E</p>
+                        </div>
+                    </div>
+                </div>
+                <h2 className="mb-1">Others</h2>
+                <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-gray-500"></div>
+                        <div className="text-center">
+                            <p>Input Placeholder</p>
+                            <p>#A3A3A3</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-gray-400"></div>
+                        <div className="text-center">
+                            <p>Input Border</p>
+                            <p>#F5F5F5</p>
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="color-box mb-1 bg-gray-100"></div>
+                        <div className="text-center">
+                            <p>Input Disabled Bg / Upsell bg</p>
+                            <p>#FAF9F9</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <h2 className="mb-1">Secondary</h2>
-            <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-secondary"></div>
-                    <div className="text-center">
-                        <p>Secondary</p>
-                        <p>#00635B</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-secondary-light"></div>
-                    <div className="text-center">
-                        <p>Secondary Light</p>
-                        <p>#E0F0E9</p>
-                    </div>
-                </div>
-            </div>
-            <h2 className="mb-1">Brand</h2>
-            <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-sh-purple"></div>
-                    <div className="text-center">
-                        <p>SH Purple</p>
-                        <p>#993366</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-sh-purple-light"></div>
-                    <div className="text-center">
-                        <p>SH Purple Light</p>
-                        <p>#FFEFFF</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-bali-bod-blue"></div>
-                    <div className="text-center">
-                        <p>Bali Bod Blue</p>
-                        <p>#213E7B</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-bali-bod-blue-light"></div>
-                    <div className="text-center">
-                        <p>Bali Bod Blue Light</p>
-                        <p>#bec5d8</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-sh-orange"></div>
-                    <div className="text-center">
-                        <p>SH Orange</p>
-                        <p>#FF9900</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-sh-orange-light"></div>
-                    <div className="text-center">
-                        <p>SH Orange Light</p>
-                        <p>#FCF6E4</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-green-light"></div>
-                    <div className="text-center">
-                        <p>Green Light</p>
-                        <p>#00B67A</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-yellow-light"></div>
-                    <div className="text-center">
-                        <p>Yellow Light</p>
-                        <p>#f9eeca</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-pink-light"></div>
-                    <div className="text-center">
-                        <p>Pink Light</p>
-                        <p>#FFF2F4</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-suncare-blue"></div>
-                    <div className="text-center">
-                        <p>Suncare Blue</p>
-                        <p>#3880DE</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-skincare-orange"></div>
-                    <div className="text-center">
-                        <p>Skincare Orange</p>
-                        <p>#E85862</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-pink-product"></div>
-                    <div className="text-center">
-                        <p>Pink Product</p>
-                        <p>#FFF4F6</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-skin-peach"></div>
-                    <div className="text-center">
-                        <p>Skin Peach</p>
-                        <p>#F2D4D7</p>
-                    </div>
-                </div>
-            </div>
-            <h2 className="mb-1">Body</h2>
-            <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-body-color"></div>
-                    <div className="text-center">
-                        <p>Body</p>
-                        <p>#151515</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-gray-600"></div>
-                    <div className="text-center">
-                        <p>Secondary Body</p>
-                        <p>#4E4E4E</p>
-                    </div>
-                </div>
-            </div>
-            <h2 className="mb-1">Others</h2>
-            <div className="grid lg:grid-cols-6 grid-cols-2 lg:gap-3 gap-[15px]">
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-gray-500"></div>
-                    <div className="text-center">
-                        <p>Input Placeholder</p>
-                        <p>#A3A3A3</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-gray-400"></div>
-                    <div className="text-center">
-                        <p>Input Border</p>
-                        <p>#F5F5F5</p>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <div className="color-box mb-1 bg-gray-100"></div>
-                    <div className="text-center">
-                        <p>Input Disabled Bg / Upsell bg</p>
-                        <p>#FAF9F9</p>
-                    </div>
-                </div>
-            </div>
-		</div>
+        </div>
 	);
 };
 
