f97cec6fba34071ff1abfbd9a2d6281ee0981fba
pebiantara <pebiantara@gmail.com>
2024-04-03 13:53:22 +0700

    [86ep0xetf] update form and button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ const Button = (props) => {
     const {id, label, type, lg, buttonClass, onClick} = props
     return (
         <button onClick={onClick} id={id} type={type ?? 'button'}
-            className={`${buttonClass ?? 'bg-primary text-white hover:bg-primary-dark font-normal'} ${lg ?? 'py-g px-4 leading-tight'} text-base inline-block align-middle text-center select-none border whitespace-no-wrap rounded-lg py-1 px-2 leading-normal no-underline`}>
+            className={`${buttonClass ?? 'btn-primary'} ${lg ?? 'btn-lg'} btn`}>
             {label}
         </button>
     )
