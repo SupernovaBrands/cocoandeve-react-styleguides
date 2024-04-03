7ba68aff400b216505b7b3d436b61e585495ea19
Teddy Maulana <maulanateddy99@gmail.com>
2024-04-03 14:52:56 +0700

    update result card

--- src/compounds/result-card.tsx
+++ src/compounds/result-card.tsx
@@ -1,29 +1,30 @@
 import FiveStars from '../../src/images/icons/five-stars.svg';
 import Link from 'next/link';
+import Badge from '../badge';
 
 const ResultCard = (props: any) => {
-    return (
-        <div className={`${props.useCarousel ? 'carousel-item col-9 lg:w-1/4' : 'lg:w-1/4'} result-card overflow-hidden  ${props.useCarousel && props.activeIndex === props.item.index ? 'active ' : ''} ${props.itemMovingNext ? 'carousel-item-next carousel-item-start ' : ''} ${props.itemMovingPrev ? 'carousel-item-prev carousel-item-end ' : ''}`}>
-            <picture>
-                <source srcSet={props.item.srcSet} media="(min-width: 992px)"/>
-                <img className="w-full" alt="/" src={props.item.src} />
-            </picture>
-            <div className="p-2 bg-white h-100">
-                <p className="flex justify-between items-center mb-0">
-                    <FiveStars className="svg--current-color h-[1em] text-primary text-base mb-0" />
-                    <span className="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded bg-blue text-white mb-1 mt-1">Body</span>
-                </p>
-                <p className='mb-1'>
-                    <strong>Product:&nbsp;</strong>
-                    <Link href="#" title={props.item.title} className="text-underline">
-                        {props.item.title}
-                    </Link>
-                </p>
-                <p className='mb-1'>"{props.item.comment}"</p>
-                <p className="underline font-bold">{props.item.author}</p>
-            </div>
-        </div>
-    );
+	return (
+		<div className={`${props.wrapper} overflow-hidden`}>
+			<picture className='rounded-tl-lg rounded-br-0 rounded-tr-lg rounded-bl-0 overflow-hidden block'>
+				<source srcSet={props.item.srcSet} media="(min-width: 992px)"/>
+				<img className="w-full" alt="/" src={props.item.src} />
+			</picture>
+			<div className="p-2 pt-0 bg-white h-100">
+				<p className="flex justify-between items-center mb-0">
+					<FiveStars className="svg--current-color h-[1em] text-primary text-lg mb-0" />
+					<span className="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded bg-sh-purple text-white mb-1 mt-1">Body</span>
+				</p>
+				<p className='mb-1'>
+					Product:&nbsp;
+					<Link href="#" title={props.item.title} className="text-underline">
+						{props.item.title}
+					</Link>
+				</p>
+				<p className='mb-g'>"{props.item.comment}"</p>
+				<p className="underline font-bold">{props.item.author}</p>
+			</div>
+		</div>
+	);
 };
 
 export default ResultCard;
