b0e7d9283fe6e3c596ba0c8227859c657dfa971f
Teddy Maulana <maulanateddy99@gmail.com>
2024-04-03 15:03:28 +0700

    update footer newsletter

--- src/compounds/footer-newsletter-form.tsx
+++ src/compounds/footer-newsletter-form.tsx
@@ -1,14 +1,14 @@
 const FooterNewsletterForm = (props) => {
-    return (
-        <form className="w-full">
-            <div className="relative flex items-stretch w-full mb-1 lg:mb-3">
-                <input type="text" className="focus:outline-none focus:border-gray-400 active:border-gray-400  focus-visible:border-gray-400 block appearance-none w-full px-[1em] py-[.875em] mb-0 text-base leading-base bg-gray-400 border border-[solid] border-gray-400 text-gray-800 border-gray-200 rounded-tl rounded-bl" placeholder="Enter your email" aria-label="Enter your email" />
-                <div className="input-group-append flex -ml-[1px]">
-                    <button className="pl-3 pr-3 leading-base font-bold inline-block align-middle text-center select-none border whitespace-no-wrap px-[1em] py-[.875em] no-underline bg-primary border-primary text-white  rounded-tl-0 rounded-bl-0 rounded-tr rounded-br" type="button">Subscribe</button>
-                </div>
-            </div>
-        </form>
-    );  
+	return (
+		<form className="w-full">
+			<div className="relative flex items-stretch w-full mb-1 lg:mb-3">
+				<input type="text" className="placeholder-gray-600 focus:outline-none focus:border-gray-400 active:border-gray-400  focus-visible:border-gray-400 block appearance-none w-full px-g py-g mb-0 text-base leading-base bg-gray-400 border border-[solid] border-gray-400 text-gray-800 border-gray-200 rounded" placeholder="Enter your email" aria-label="Enter your email" />
+				<div className="input-group-append flex -ml-[1px]">
+					<button className="pl-3 pr-3 leading-base font-bold inline-block align-middle text-center select-none border whitespace-no-wrap no-underline bg-primary border-primary text-white  rounded" type="button">Subscribe</button>
+				</div>
+			</div>
+		</form>
+	);  
 };
 
 export default FooterNewsletterForm;
