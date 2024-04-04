6f5c894031d3e07d64ccb818c6ea22e928344944
Awais Gill <awais@supernovabrands.com>
2024-04-05 00:03:16 +0500

    task/86ep0xerv Buttons

--- pages/components/buttons.tsx
+++ pages/components/buttons.tsx
@@ -7,35 +7,35 @@ export default function Buttons() {
 	return (
 		<div className="container my-4">
 			<h1 className="mb-1">Buttons</h1>
-			<Button label="Button" lg={false} buttonClass="bg-primary hover:bg-primary-darken border-transparent text-white"></Button>
+			<Button label="Button" lg={false} buttonClass="bg-primary hover:bg-primary-darken border-transparent text-white">Button</Button>
 			<br />
-			<Button label="Button" lg={false} buttonClass="bg-transparent hover:bg-primary hover:text-white border-primary text-primary"></Button>
+			<Button label="Button" lg={false} buttonClass="bg-transparent hover:bg-primary hover:text-white border-primary text-primary">Button</Button>
 			<br />
-			<Button label="Button a tag" lg={false} buttonClass="bg-primary hover:bg-primary-darken border-transparent text-white"></Button>
+			<Button label="Button a tag" lg={false} buttonClass="bg-primary hover:bg-primary-darken border-transparent text-white">Button a tag</Button>
 			<br />
 			<Button label="Button With icon" lg={false} buttonClass="bg-primary hover:bg-primary-darken border-transparent text-white inline-flex items-center mb-2">
-				<Search className="h-[12px] ml-[5px]" fill="white"/>
+				Button With icon <Search className="h-[12px] ml-[5px]" fill="white"/>
 			</Button>
 			<br />
 			<Button label="Shop" lg={false} buttonClass="bg-primary hover:bg-primary-darken border-transparent text-white inline-flex items-center mb-2">
-				<ChevronDown className="h-[12px] ml-[5px]" fill="white"/>
+				Shop <ChevronDown className="h-[12px] ml-[5px]" fill="white"/>
 			</Button>
 			<br />
-			<Button label="Button Large" buttonClass="bg-primary hover:bg-primary-darken border-primary text-white"></Button>
+			<Button label="Button Large" buttonClass="bg-primary hover:bg-primary-darken border-primary text-white">Button Large</Button>
 			<br />
 			<Button buttonClass="bg-primary hover:bg-primary-darken border-transparent">
 				<Spin className="animate-spin h-[16px] w-[16px] text-white" fill="white"/>
 			</Button>
 			<br />
-			<Button label="Button Large" buttonClass="bg-transparent hover:bg-primary hover:text-white border-primary text-primary"></Button>
+			<Button label="Button Large" buttonClass="bg-transparent hover:bg-primary hover:text-white border-primary text-primary">Button Large</Button>
 			<br />
-			<Button label="Button Block" lg={false} buttonClass="bg-primary hover:bg-primary-darken w-full border-transparent text-white"></Button>
+			<Button label="Button Block" lg={false} buttonClass="bg-primary hover:bg-primary-darken w-full border-transparent text-white">Button Block</Button>
 			<br />
-			<Button label="Button Block" lg={false} buttonClass="bg-transparent hover:bg-primary hover:text-white w-full border-primary text-primary"></Button>
+			<Button label="Button Block" lg={false} buttonClass="bg-transparent hover:bg-primary hover:text-white w-full border-primary text-primary">Button Block</Button>
 			<br />
-			<Button label="Button Large Block" buttonClass="bg-primary hover:bg-primary-darken w-full border-transparent text-white"></Button>
+			<Button label="Button Large Block" buttonClass="bg-primary hover:bg-primary-darken w-full border-transparent text-white">Button Large Block</Button>
 			<br />
-			<Button label="Button Large Block" buttonClass="bg-transparent hover:bg-primary hover:text-white w-full border-primary text-primary"></Button>
+			<Button label="Button Large Block" buttonClass="bg-transparent hover:bg-primary hover:text-white w-full border-primary text-primary">Button Large Block</Button>
 		</div>
 	);
 }
