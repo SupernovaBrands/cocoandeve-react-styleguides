07844d39a5fa2d66e36ab23e911990cd385f941f
Awais Gill <awais@supernovabrands.com>
2024-04-14 23:48:53 +0500

    task/86ep0xfab Testimonials Fix

--- src/sections/Editors.tsx
+++ src/sections/Editors.tsx
@@ -1,40 +1,44 @@
-import Testimonial from '@/sections/Testimonial';
-
 const Editors = () => {
     return (
         <section className="list-logo container my-4 block">
-            <ul className="flex flex-wrap items-center lg:justify-between lg:px-g flex-nowrap mb-0">
-                <li className="h4 mb-0 col-auto lg:px-g sm:px-hg">Featured in:</li>
-                <Testimonial 
-                    srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_nylon_x19.jpg?v=1687419613"
-                    src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_nylon_x15.jpg?v=1687419613"
-                >
-                </Testimonial>
-                <Testimonial 
-                    srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_cosmopolitan_x24.jpg?v=1687419613"
-                    src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_cosmopolitan_x17.jpg?v=1687419613"
-                >
-                </Testimonial>
-                <Testimonial 
-                    srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_elle_x26.jpg?v=1687419613"
-                    src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_elle_x22.jpg?v=1687419613"
-                >
-                </Testimonial>
-                <Testimonial 
-                    srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_glamor_x26.jpg?v=1687419613"
-                    src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_glamor_x17.jpg?v=1687419613"
-                >
-                </Testimonial>
-                <Testimonial 
-                    srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_refinery_x40.jpg?v=1687419613"
-                    src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_refinery_x36.jpg?v=1687419613"
-                >
-                </Testimonial>
-                <Testimonial 
-                    srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_mail-online_x26.jpg?v=1687419613"
-                    src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_mail-online_x17.jpg?v=1687419613"
-                >
-                </Testimonial>
+            <ul className="flex flex-wrap items-center lg:justify-between lg:px-g flex-nowrap mb-0 lg:mx-[-15px] sm:mx-[-7.5px]">
+                <li className="col-auto flex-[0_0_auto] w-auto max-w-full h4 mb-0 relative lg:px-g sm:px-hg">Featured in:</li>
+                <li className="col-auto flex-[0_0_auto] w-auto max-w-full relative lg:px-g sm:px-hg">
+                    <picture>
+                        <source srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_nylon_x19.jpg?v=1687419613" media="(min-width: 992px)" />
+                        <img className="w-full align-middle inline" src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_nylon_x15.jpg?v=1687419613" loading="lazy" />
+                    </picture>
+                </li>
+                <li className="col-auto flex-[0_0_auto] w-auto max-w-full relative lg:px-g sm:px-hg">
+                    <picture>
+                        <source srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_cosmopolitan_x24.jpg?v=1687419613" media="(min-width: 992px)" />
+                        <img className="w-full align-middle inline" src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_cosmopolitan_x17.jpg?v=1687419613" loading="lazy" />
+                    </picture>
+                </li>
+                <li className="col-auto flex-[0_0_auto] w-auto max-w-full relative lg:px-g sm:px-hg">
+                    <picture>
+                        <source srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_elle_x26.jpg?v=1687419613" media="(min-width: 992px)" />
+                        <img className="w-full align-middle inline" src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_elle_x22.jpg?v=1687419613" loading="lazy" />
+                    </picture>
+                </li>
+                <li className="col-auto flex-[0_0_auto] w-auto max-w-full relative lg:px-g sm:px-hg">
+                    <picture>
+                        <source srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_glamor_x26.jpg?v=1687419613" media="(min-width: 992px)" />
+                        <img className="w-full align-middle inline" src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_glamor_x17.jpg?v=1687419613" loading="lazy" />
+                    </picture>
+                </li>
+                <li className="col-auto flex-[0_0_auto] w-auto max-w-full relative lg:px-g sm:px-hg">
+                    <picture>
+                        <source srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_refinery_x40.jpg?v=1687419613" media="(min-width: 992px)" />
+                        <img className="w-full align-middle inline" src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_refinery_x36.jpg?v=1687419613" loading="lazy" />
+                    </picture>
+                </li>
+                <li className="col-auto flex-[0_0_auto] w-auto max-w-full relative lg:px-g sm:px-hg">
+                    <picture>
+                        <source srcSet="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_mail-online_x26.jpg?v=1687419613" media="(min-width: 992px)" />
+                        <img className="w-full align-middle inline" src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/pr_mail-online_x17.jpg?v=1687419613" loading="lazy" />
+                    </picture>
+                </li>
             </ul>
         </section>
     )
