8b77035488c3132edd06edeccbd5d3650cc7823b
Awais Gill <awais@supernovabrands.com>
2024-04-18 01:20:30 +0500

    task/86ep0xgtk Blog Filtered

--- src/sections/blog-post.tsx
+++ src/sections/blog-post.tsx
@@ -1,7 +1,7 @@
 const BlogPostSection = (props) => {
 	return (
         <article className="w-full lg:w-1/2 post-card mb-4 lg:mb-2 lg:px-g sm:px-hg">
-            <figure className="mb-2 no-gutters__in-container">
+            <figure className="mb-2 no-gutters__in-container sm:-mx-g lg:mx-0">
                 <a href={props.item.link} className="hover-overlay block">
                     <picture>
                         <source srcSet={props.item.srcSet} media="(min-width: 992px)" />
