f8898db27958db87a639c5b55efaa8aaf5ee1550
Teddy Maulana <maulanateddy99@gmail.com>
2024-05-19 22:42:50 +0700

    general updates

--- src/components/InstagramCard.tsx
+++ src/components/InstagramCard.tsx
@@ -1,12 +1,12 @@
 const InstagramCard = (props: any) => {
-    const { videoUrl, author, title, url, classes } = props;
+    const { videoUrl, author, title, url, classes, product } = props;
     return (
         <div className={`text-left rounded ${classes}`}>
             <video preload={props.index === 1 ? 'metadata' : 'none'} className="w-full rounded mb-g lazy-video" muted={true} loop={true} webkit-playsinline="true" autoPlay={true}>
                 <source src={videoUrl} type="video/mp4" />
             </video>
             <p className="font-bold mb-25">{author}</p>
-            <a href={url || '/'} className="font-size-sm text-body"><u>{title}</u></a>
+            <a href={`/products/${product}`} className="font-size-sm text-body"><u>{title}</u></a>
         </div>
     );
 };
