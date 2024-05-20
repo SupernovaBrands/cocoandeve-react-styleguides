65406b3dfc94ea19ab458da3f8e58c45c4fdd7a7
Teddy Maulana <maulanateddy99@gmail.com>
2024-05-20 22:51:05 +0700

    update cart

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -27,10 +27,11 @@ interface Props {
 	strapiCartSetting: any;
 	onUpdateCart: (item: any, qty: number) => void;
 	onDeleteLine: (lineId: string) => void;
+	tierDiscountSettings: any;
 }
 
 const Cart: React.FC<Props> = (props) => {
-	const { showCart, cartCount, cartData, itemCount, onUpdateCart, onDeleteLine } = props;
+	const { showCart, cartData, itemCount, onUpdateCart, onDeleteLine, tierDiscountSettings } = props;
 	// const storeApi = new storefrontApi();
 	const [loadingInit, setLoadingInit] = useState(props.isLoading);
 	const [cart, setCart] = useState({
@@ -82,110 +83,10 @@ const Cart: React.FC<Props> = (props) => {
 
 	useEffect(() => {
 		if (cartData) {
-			const modelizedCart = cartModel(cartData);
-			setCart({ ...modelizedCart });
-			console.log('modelizedCart', modelizedCart);
+			setCart({ ...cartData });
+			console.log('cartData123', cartData);
 		}
 	}, [cartData, itemCount]);
-
-	const getVariantOptions = (item: any) => {
-		const { merchandise: { product: { options } } } = item;
-		const swatches = options.filter((opt) => opt.name.toLowerCase().includes('color')
-			|| opt.name.toLowerCase().includes('style')
-			|| opt.name.toLowerCase().includes('scent')
-			|| opt.name.toLowerCase().includes('tangle tamer')).map((opt) => {
-			let { name } = opt;
-			if (name.toLowerCase().includes('drops') || name.toLowerCase().includes('foam') || name.toLowerCase().includes('color')) {
-				name = 'Shade';
-			} else if (name.toLowerCase().includes('style')) {
-				name = 'Style';
-			} else if (name.toLowerCase().includes('scent')) {
-				name = 'Scent';
-			} else if (name.toLowerCase().includes('tangle tamer')) {
-				// tangle tamer text already applied from variant name, so its set to empty
-				name = '';
-			}
-			return { ...opt, name };
-		});
-
-		return swatches;
-	}
-
-	const cartModel = (cart: any) => {
-		let data = { ...cart };
-		if (data && data.lines) {
-			let currentTotal = 0;
-			// eslint-disable-next-line no-return-assign
-			data.lines.forEach((item) => currentTotal += item.quantity);
-
-			data.items = data.lines.map((item: any) => {
-				const model = {...item};
-				model.isManualGwp = false;
-				model.isAutoGwp = false;
-				const checkAttributes = item.attributes.find((i) => i.key === '_campaign_type' && i.value === 'manual_gwp');
-				if (checkAttributes) {
-					model.isManualGwp = true;
-					if (currentTotal === data.totalQuantity) { data.totalQuantity -= model.quantity; }
-				}
-				const autoAttributes = model.attributes.find((i) => i.key === '_campaign_type' && i.value === 'auto_gwp');
-				if (autoAttributes) {
-					model.isAutoGwp = true;
-				}
-
-				model.diffPriceBundle = 0;
-				model.comparePrice = 0;
-				model.originalPrice = parseFloat(item.cost.amountPerQuantity.amount) * 100;
-				if (item.cost && item.cost.compareAtAmountPerQuantity) {
-					if (item.sellingPlanAllocation && item.merchandise.compareAtPrice && item.merchandise.compareAtPrice.amount) {
-						model.comparePrice = parseFloat(item.merchandise.compareAtPrice.amount) * 100;
-						model.diffPriceBundle = model.comparePrice - model.originalPrice;
-					} else {
-						model.comparePrice = parseFloat(item.cost.compareAtAmountPerQuantity.amount) * 100;
-						model.diffPriceBundle = model.comparePrice - model.originalPrice;
-					}
-				}
-				model.isFreeItem = item.cost.totalAmount.amount === '0.0';
-				model.swatches = getVariantOptions(item);
-				model.variants = item.merchandise.product.variants.edges.map((variant) => variant.node);
-				model.selectedSwatch = item.merchandise.selectedOptions.filter((opt) => opt.name.toLowerCase() !== 'size').map((opt) => opt.value);
-
-				model.showPreorderNotif = false;
-				model.showPreorderNotif_2 = false;
-				model.showPreorderNotif_3 = false;
-				model.featuredImageUrl = productImages.find((img: any) => img.handle === item.merchandise.product.handle)
-					? productImages.find((img) => img.handle === item.merchandise.product.handle).featured_image_url : null;
-				model.totalDiscountAmount = 0;
-				if (model.discountAllocations && model.discountAllocations.length) {
-					model.discountAllocations.forEach((discount) => {
-						model.totalDiscountAmount += (parseFloat(model.discountedAmount.amount) * 100) / model.quantity;
-					});
-				}
-				model.priceAfterDiscounted = model.originalPrice - model.totalDiscountAmount;
-
-				return model;
-			});
-			data.items = data.items.reverse();
-		}
-
-		const subtotalTemp = calculateSubTotal(cartData.items);
-		console.log('subtotalTemp', subtotalTemp);
-		if (cartData.items) {
-			cartData.subtotalPrice = calculateSubTotal(cartData.items);
-		}
-		return data;
-	}
-
-	const calculateSubTotal = (items: Array<any>) => {
-		let subTotal = 0;
-		if (items && items.length > 0) {
-			items.forEach((item) => {
-				if (!item.isManualGwp) {
-					subTotal += item.comparePrice ? item.comparePrice * item.quantity : item.originalPrice * item.quantity;
-				}
-			});
-		}
-		return subTotal;
-	}
 
 	const onApplyDiscountCode = () => {
 
@@ -277,7 +178,7 @@ const Cart: React.FC<Props> = (props) => {
 								<SvgClose className="svg w-[1em]" aria-hidden="true" />
 							</button>
 
-							{tSettings.cartDiscountMeter && !tSettings.cartDiscountMeter.enable
+							{tierDiscountSettings && !tierDiscountSettings.enable
 								&& tSettings.cartShippingMeter.enable
 								&& shippingMeter
 								&& shippingMeter.enabled
@@ -288,8 +189,8 @@ const Cart: React.FC<Props> = (props) => {
 										current={shippingMeter.current}
 									/>
 								)}
-							{tSettings.cartDiscountMeter && tSettings.cartDiscountMeter.enable && discountMeter
-								&& discountMeter.enabled && itemCount > 0 && (
+							{tierDiscountSettings && tierDiscountSettings.enable && discountMeter
+								&& discountMeter.enabled && cart?.itemCount > 0 && (
 								<CartDiscountMeter
 									target={discountMeter.target}
 									current={discountMeter.current}
