2ffb49bcba12414aada6cf808281979b8fd58c5e
Awais Gill <awais@supernovabrands.com>
2024-05-24 01:52:46 +0500

    Real Result Page updates

--- src/templates/RealResults.tsx
+++ src/templates/RealResults.tsx
@@ -6,6 +6,7 @@ import TestimonialCarousel from '~/sections/TestimonialCarousel';
 import ProductCarousel from '~/sections/ProductCarousel';
 import REVIEWS_PH from '~/modules/reviews';
 import Instagram from '~/sections/Instagram';
+import Service from '~/sections/Service';
 import YotpoReviews from '~/components/yotpo-review-widget';
 import TabNav from '~/components/TabNav';
 import TabContent from '~/components/TabContent';
@@ -129,7 +130,7 @@ const RealResults = () => {
 	const [showMore, setShowMore] = useState(true);
 	const [page, setPage] = useState(1);
 	const [range, setRange] = useState('all');
-    const [activeTab2, setActiveTab2] = useState('tan');
+    const [activeTab2, setActiveTab2] = useState('all');
 	const [activeTab, setActiveTab] = useState('tan');
 
     let store = 'us';
@@ -249,8 +250,8 @@ const RealResults = () => {
     return (
 		<>
 			<div className='container py-4'>
-				<div className="mb-3 flex flex-wrap  items-center justify-center">
-					<h1 className='text-center mb-2 lg:mb-3 block w-full'>Real Customers. Real Reviews</h1>
+				<h1 className='text-center mb-2 lg:mb-4 block w-full'>Real Customers. Real Reviews</h1>
+				<div className="mb-3 flex flex-wrap  items-center justify-center lg:-mx-g sm:-mx-hg">
 					<label htmlFor="real-result__select" className="text-base font-normal mr-3 mb-0 lg:text-lg">Filter By:</label>
 					<select id="real-result__select" className="border-body bg-gray-400 custom-select rounded w-2/5 lg:hidden" aria-labelledby="real-result__select" onChange={changeDrop}>
 						<option value="all" selected={true}>All</option>
@@ -280,11 +281,11 @@ const RealResults = () => {
 						})}
 					</ul>
 				</div>
-				<div className="flex flex-wrap">
+				<div className="flex flex-wrap lg:-mx-g sm:-mx-hg">
 					{data.length > 0 && data.map((item) => (<RealResultCard data={item} />))}
 				</div>
 				{showMore && (
-					<div className="flex flex-wrap justify-center">
+					<div className="flex flex-wrap justify-center lg:-mx-g sm:-mx-hg">
 						<div className="col text-center">
 							<a href="#!" className={`bg-transparent hover:bg-primary hover:text-white border-primary text-primary btn-lg btn block hover:no-underline ${showMore}`} onClick={loadMore}>
 								Show more reviews
@@ -293,71 +294,67 @@ const RealResults = () => {
 					</div>
 				)}
 			</div>
-			<div className="pb-2">
-				<div className="container">
-					<div className="row px-1">
-						<h2 className="h1 mb-2 w-full text-center">Coco & Eve’s Customer Reviews</h2>
-						<div className="flex flex-wrap justify-center lg:-mx-g sm:-mx-hg">
-							<ul id="real-result__yotpo-tab" className="flex-wrap list-none pl-0 mb-3 text-center flex" role="tablist">
-								<li className="nav-item" role="presentation">
-									<TabNav className={`lg:px-3 sm:px-1 w-auto block no-underline hover:no-underline lg:text-lg sm:text-[1rem] mb-0 border p-0 leading-[2.3!important] w-[5.625em] rounded-tl-[6px] rounded-br-[0] rounded-tr-[0] rounded-bl-[6px] border-body ${activeTab === 'tan' ? ' bg-primary text-white hover:text-white' : 'bg-white text-body'} `} title='Tan & SPF' active={activeTab === 'tan'} onNavChange={() => setActiveTab('tan')} />
-								</li>
-								<li className="nav-item" role="presentation">
-									<TabNav className={`block no-underline hover:no-underline lg:text-lg sm:text-[1rem] mb-0 border border-l-0 p-0 leading-[2.3!important] lg:w-[5.625em] sm:w-[4.625em] border-body ${activeTab === 'hair' ? ' bg-primary text-white hover:text-white' : 'bg-white text-body'}`} title='Hair' active={activeTab === 'hair'} onNavChange={() => setActiveTab('hair')} />
-								</li>
-								<li className="nav-item" role="presentation">
-									<TabNav className={`block no-underline hover:no-underline lg:text-lg sm:text-[1rem] mb-0 border border-l-0 p-0 leading-[2.3!important] lg:w-[5.625em] sm:w-[4.625em] border-body ${activeTab === 'skin' ? ' bg-primary text-white hover:text-white' : 'bg-white text-body'}`} title='Skin' active={activeTab === 'skin'} onNavChange={() => setActiveTab('skin')} />
-								</li>
-								<li className="nav-item" role="presentation">
-									<TabNav className={`block no-underline hover:no-underline lg:text-lg sm:text-[1rem] mb-0 border border-l-0 p-0 leading-[2.3!important] lg:w-[5.625em] sm:w-[4.625em] border-body rounded-bl-[0] rounded-tl-[0] rounded-br-[6px] rounded-tr-[6px] ${activeTab === 'body' ? ' bg-primary text-white hover:text-white' : 'bg-white text-body'}`} title='Body' active={activeTab === 'body'} onNavChange={() => setActiveTab('body')} />
-								</li>
-							</ul>
-						</div>
-						<TabContent active={activeTab === 'tan'}>
-							<YotpoReviews
-								productId={4543113265187}
-								productName='Sunny Honey Bali Bronzing Foam'
-								productUrl='https://dev.cocoandeve.com/products/sunny-honey-bali-bronzing-self-tan-mousse'
-								productImage='//cdn.shopify.com/s/files/1/0286/1327/9779/products/FOAM_DARK_YELLOW_1_large.jpg%3Fv=1609922006'
-								productDesc='<div> <strong></strong>Anti-cellulite, anti-ageing self tan with zero nasties. Created using unique CellushapeTM formula to hydrate and firm the skin.<br> </div> <ul> <li>Blurs Pigmentation and Perfects Skin.</li> <li>Tropical Mango and Guava Scent. (No biscuit smells!)</li> <li>Lightweight, non-sticky formula.</li> <li>Fast drying and develops in just 2 hours</li> <li>Vegan. 100% Natural DHA. No Nasties</li> </ul>'
-								productSkus='CE0000072020,CE0000072040,CE0000072060,CE0000036020,CE0000036040,CE0000036060,CE0004012020,CE0000432040,CE0000432030,CE0002382020,CE0002382040,CE0002382060,CE0000852010,CE0000852020,CE0000852030,CE0001512020,CE0003532020,CE0000038040,CE0000102040,CE0002412020,CE0003662020,CE0003152020,CE0002522020,CE0003562020,CE0003562040,CE0003562060,CE0002842080,CE0000042060,CE0000042020,CE0001912020,CE0000052020,CE0002402020,CE0002402040,CE0000054040,CE0000054020'
-								canCreate={true}
-							/>
-						</TabContent>
-						<TabContent active={activeTab === 'hair'}>
-							<YotpoReviews
-								productId={4543113265187}
-								productName='Sunny Honey Bali Bronzing Foam'
-								productUrl='https://dev.cocoandeve.com/products/sunny-honey-bali-bronzing-self-tan-mousse'
-								productImage='//cdn.shopify.com/s/files/1/0286/1327/9779/products/FOAM_DARK_YELLOW_1_large.jpg%3Fv=1609922006'
-								productDesc='<div> <strong></strong>Anti-cellulite, anti-ageing self tan with zero nasties. Created using unique CellushapeTM formula to hydrate and firm the skin.<br> </div> <ul> <li>Blurs Pigmentation and Perfects Skin.</li> <li>Tropical Mango and Guava Scent. (No biscuit smells!)</li> <li>Lightweight, non-sticky formula.</li> <li>Fast drying and develops in just 2 hours</li> <li>Vegan. 100% Natural DHA. No Nasties</li> </ul>'
-								productSkus='CE0000062020,CE0001672020,CE0001782020,CE0000732020,CE0001242020,CE0001952020,CE0001662020,CE0002992020,CE0000332020,CE0001152020,CE0001592020,CE0002742020,CE0002122020,CE0001252020,CE0000842040,CE0002242020,CE0001272020,CE0001664020,CE0000342040,CE0003432020,CE0003512020,CE0001492020,CE0003522020,CE0003442020,CE0003012040,CE0003572020,CE0002942020,CE0003122020,CE0003112020,CE0002932020,CE0000152080,CE0000152060,CE0000152010,CE0001182020,CE0000022060,CE0000022025,CE0001902020,CE0002342020,CE0002112020,CE0002172020,CE0000312040,CE0000962040,CE0003212020,CE0001932020,CE0002452020'
-								canCreate={true}
-							/>
-						</TabContent>
-						<TabContent active={activeTab === 'skin'}>
-							<YotpoReviews
-								productId={4543113265187}
-								productName='Sunny Honey Bali Bronzing Foam'
-								productUrl='https://dev.cocoandeve.com/products/sunny-honey-bali-bronzing-self-tan-mousse'
-								productImage='//cdn.shopify.com/s/files/1/0286/1327/9779/products/FOAM_DARK_YELLOW_1_large.jpg%3Fv=1609922006'
-								productDesc='<div> <strong></strong>Anti-cellulite, anti-ageing self tan with zero nasties. Created using unique CellushapeTM formula to hydrate and firm the skin.<br> </div> <ul> <li>Blurs Pigmentation and Perfects Skin.</li> <li>Tropical Mango and Guava Scent. (No biscuit smells!)</li> <li>Lightweight, non-sticky formula.</li> <li>Fast drying and develops in just 2 hours</li> <li>Vegan. 100% Natural DHA. No Nasties</li> </ul>'
-								productSkus='CE0001612025,CE0002712020,CE0001612026,CE0001602020,CE0001612020'
-								canCreate={true}
-							/>
-						</TabContent>
-						<TabContent active={activeTab === 'body'}>
-							<YotpoReviews
-								productId={4543113265187}
-								productName='Sunny Honey Bali Bronzing Foam'
-								productUrl='https://dev.cocoandeve.com/products/sunny-honey-bali-bronzing-self-tan-mousse'
-								productImage='//cdn.shopify.com/s/files/1/0286/1327/9779/products/FOAM_DARK_YELLOW_1_large.jpg%3Fv=1609922006'
-								productDesc='<div> <strong></strong>Anti-cellulite, anti-ageing self tan with zero nasties. Created using unique CellushapeTM formula to hydrate and firm the skin.<br> </div> <ul> <li>Blurs Pigmentation and Perfects Skin.</li> <li>Tropical Mango and Guava Scent. (No biscuit smells!)</li> <li>Lightweight, non-sticky formula.</li> <li>Fast drying and develops in just 2 hours</li> <li>Vegan. 100% Natural DHA. No Nasties</li> </ul>'
-								productSkus='CE0000112040,CE0000112060,CE0002372040,CE0002372020,CE0001222020,CE0001232020,CE0000122040,CE0000022040,CE0000114075,CE0000114080,CE0000254020,CE0000312060,CE0002842080,CE0003662020,CE0002932020'
-								canCreate={true}
-							/>
-						</TabContent>
-					</div>
+			<div className="container">
+				<h2 className="h1 mb-2 w-full text-center">Coco & Eve’s Customer Reviews</h2>
+				<div className="flex flex-wrap justify-center lg:-mx-g sm:-mx-hg">
+					<ul id="real-result__yotpo-tab" className="flex-wrap list-none pl-0 mb-2 text-center flex" role="tablist">
+						<li className="nav-item" role="presentation">
+							<TabNav className={`font-normal lg:px-3 sm:px-1 w-auto block no-underline hover:no-underline lg:text-lg sm:text-[1rem] mb-0 border p-0 leading-[2.3!important] w-[5.625em] rounded-tl-[6px] rounded-br-[0] rounded-tr-[0] rounded-bl-[6px] border-body ${activeTab === 'tan' ? ' bg-primary text-white hover:text-white' : 'bg-white text-body'} `} title='Tan & SPF' active={activeTab === 'tan'} onNavChange={() => setActiveTab('tan')} />
+						</li>
+						<li className="nav-item" role="presentation">
+							<TabNav className={`font-normal block no-underline hover:no-underline lg:text-lg sm:text-[1rem] mb-0 border border-l-0 p-0 leading-[2.3!important] lg:w-[5.625em] sm:w-[5.3125em] border-body ${activeTab === 'hair' ? ' bg-primary text-white hover:text-white' : 'bg-white text-body'}`} title='Hair' active={activeTab === 'hair'} onNavChange={() => setActiveTab('hair')} />
+						</li>
+						<li className="nav-item" role="presentation">
+							<TabNav className={`font-normal block no-underline hover:no-underline lg:text-lg sm:text-[1rem] mb-0 border border-l-0 p-0 leading-[2.3!important] lg:w-[5.625em] sm:w-[5.3125em] border-body ${activeTab === 'skin' ? ' bg-primary text-white hover:text-white' : 'bg-white text-body'}`} title='Skin' active={activeTab === 'skin'} onNavChange={() => setActiveTab('skin')} />
+						</li>
+						<li className="nav-item" role="presentation">
+							<TabNav className={`font-normal block no-underline hover:no-underline lg:text-lg sm:text-[1rem] mb-0 border border-l-0 p-0 leading-[2.3!important] lg:w-[5.625em] sm:w-[5.3125em] border-body rounded-bl-[0] rounded-tl-[0] rounded-br-[6px] rounded-tr-[6px] ${activeTab === 'body' ? ' bg-primary text-white hover:text-white' : 'bg-white text-body'}`} title='Body' active={activeTab === 'body'} onNavChange={() => setActiveTab('body')} />
+						</li>
+					</ul>
+					<TabContent active={activeTab === 'tan'} className="px-g">
+						<YotpoReviews
+							productId={4543113265187}
+							productName='Sunny Honey Bali Bronzing Foam'
+							productUrl='https://dev.cocoandeve.com/products/sunny-honey-bali-bronzing-self-tan-mousse'
+							productImage='//cdn.shopify.com/s/files/1/0286/1327/9779/products/FOAM_DARK_YELLOW_1_large.jpg%3Fv=1609922006'
+							productDesc='<div> <strong></strong>Anti-cellulite, anti-ageing self tan with zero nasties. Created using unique CellushapeTM formula to hydrate and firm the skin.<br> </div> <ul> <li>Blurs Pigmentation and Perfects Skin.</li> <li>Tropical Mango and Guava Scent. (No biscuit smells!)</li> <li>Lightweight, non-sticky formula.</li> <li>Fast drying and develops in just 2 hours</li> <li>Vegan. 100% Natural DHA. No Nasties</li> </ul>'
+							productSkus='CE0000072020,CE0000072040,CE0000072060,CE0000036020,CE0000036040,CE0000036060,CE0004012020,CE0000432040,CE0000432030,CE0002382020,CE0002382040,CE0002382060,CE0000852010,CE0000852020,CE0000852030,CE0001512020,CE0003532020,CE0000038040,CE0000102040,CE0002412020,CE0003662020,CE0003152020,CE0002522020,CE0003562020,CE0003562040,CE0003562060,CE0002842080,CE0000042060,CE0000042020,CE0001912020,CE0000052020,CE0002402020,CE0002402040,CE0000054040,CE0000054020'
+							canCreate={true}
+						/>
+					</TabContent>
+					<TabContent active={activeTab === 'hair'} className="px-g">
+						<YotpoReviews
+							productId={4543113265187}
+							productName='Sunny Honey Bali Bronzing Foam'
+							productUrl='https://dev.cocoandeve.com/products/sunny-honey-bali-bronzing-self-tan-mousse'
+							productImage='//cdn.shopify.com/s/files/1/0286/1327/9779/products/FOAM_DARK_YELLOW_1_large.jpg%3Fv=1609922006'
+							productDesc='<div> <strong></strong>Anti-cellulite, anti-ageing self tan with zero nasties. Created using unique CellushapeTM formula to hydrate and firm the skin.<br> </div> <ul> <li>Blurs Pigmentation and Perfects Skin.</li> <li>Tropical Mango and Guava Scent. (No biscuit smells!)</li> <li>Lightweight, non-sticky formula.</li> <li>Fast drying and develops in just 2 hours</li> <li>Vegan. 100% Natural DHA. No Nasties</li> </ul>'
+							productSkus='CE0000062020,CE0001672020,CE0001782020,CE0000732020,CE0001242020,CE0001952020,CE0001662020,CE0002992020,CE0000332020,CE0001152020,CE0001592020,CE0002742020,CE0002122020,CE0001252020,CE0000842040,CE0002242020,CE0001272020,CE0001664020,CE0000342040,CE0003432020,CE0003512020,CE0001492020,CE0003522020,CE0003442020,CE0003012040,CE0003572020,CE0002942020,CE0003122020,CE0003112020,CE0002932020,CE0000152080,CE0000152060,CE0000152010,CE0001182020,CE0000022060,CE0000022025,CE0001902020,CE0002342020,CE0002112020,CE0002172020,CE0000312040,CE0000962040,CE0003212020,CE0001932020,CE0002452020'
+							canCreate={true}
+						/>
+					</TabContent>
+					<TabContent active={activeTab === 'skin'} className="px-g">
+						<YotpoReviews
+							productId={4543113265187}
+							productName='Sunny Honey Bali Bronzing Foam'
+							productUrl='https://dev.cocoandeve.com/products/sunny-honey-bali-bronzing-self-tan-mousse'
+							productImage='//cdn.shopify.com/s/files/1/0286/1327/9779/products/FOAM_DARK_YELLOW_1_large.jpg%3Fv=1609922006'
+							productDesc='<div> <strong></strong>Anti-cellulite, anti-ageing self tan with zero nasties. Created using unique CellushapeTM formula to hydrate and firm the skin.<br> </div> <ul> <li>Blurs Pigmentation and Perfects Skin.</li> <li>Tropical Mango and Guava Scent. (No biscuit smells!)</li> <li>Lightweight, non-sticky formula.</li> <li>Fast drying and develops in just 2 hours</li> <li>Vegan. 100% Natural DHA. No Nasties</li> </ul>'
+							productSkus='CE0001612025,CE0002712020,CE0001612026,CE0001602020,CE0001612020'
+							canCreate={true}
+						/>
+					</TabContent>
+					<TabContent active={activeTab === 'body'} className="px-g">
+						<YotpoReviews
+							productId={4543113265187}
+							productName='Sunny Honey Bali Bronzing Foam'
+							productUrl='https://dev.cocoandeve.com/products/sunny-honey-bali-bronzing-self-tan-mousse'
+							productImage='//cdn.shopify.com/s/files/1/0286/1327/9779/products/FOAM_DARK_YELLOW_1_large.jpg%3Fv=1609922006'
+							productDesc='<div> <strong></strong>Anti-cellulite, anti-ageing self tan with zero nasties. Created using unique CellushapeTM formula to hydrate and firm the skin.<br> </div> <ul> <li>Blurs Pigmentation and Perfects Skin.</li> <li>Tropical Mango and Guava Scent. (No biscuit smells!)</li> <li>Lightweight, non-sticky formula.</li> <li>Fast drying and develops in just 2 hours</li> <li>Vegan. 100% Natural DHA. No Nasties</li> </ul>'
+							productSkus='CE0000112040,CE0000112060,CE0002372040,CE0002372020,CE0001222020,CE0001232020,CE0000122040,CE0000022040,CE0000114075,CE0000114080,CE0000254020,CE0000312060,CE0002842080,CE0003662020,CE0002932020'
+							canCreate={true}
+						/>
+					</TabContent>
 				</div>
 			</div>
 			<div className='w-full bg-gray-100 mt-4 py-4'>
@@ -365,6 +362,7 @@ const RealResults = () => {
 			</div>
 			<ProductCarousel products={PRODUCTS} isStyleguide={true} />
 			<Instagram className="bg-pink-light" isStyleguide={true} />
+			<Service />
 		</>
     );
 }
