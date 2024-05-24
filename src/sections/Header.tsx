e5be724f4677e37efb303f4d1f76c0fa26937376
Teddy Maulana <maulanateddy99@gmail.com>
2024-05-24 10:54:41 +0700

    update tooltip

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -9,6 +9,7 @@ import CartIcon from '~/images/icons/cart.svg';
 import SearchBox from '~/compounds/SearchBox';
 import AccountDropdown from '~/compounds/AccountDropdown';
 import NavMegaMenuAll from '~/compounds/NavMegaMenuAll';
+import Tooltip from '~/components/Tooltip';
 import { useRouter } from 'next/navigation';
 import PalmTree from '~/images/icons/palm-tree-v2.svg';
 import { getCustomersBalance } from '~/modules/swell/redemption';
@@ -24,6 +25,7 @@ const Header = (props: any) => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const [sevenDaysSalesIds, setSevenDaysSalesIds] = useState([]);
 	const [userPts, setUserPts] = useState(0);
+	const [tooltipShow, setTooltipShow] = useState(true);
 	const router = useRouter();
 	const onToggleMobileNav = () => {
 		setOpenDrawer(!openDrawer);
@@ -43,6 +45,10 @@ const Header = (props: any) => {
 
 	const handleClose = (): void => { // Add type annotation for function
 		setOpenCartDrawer(false);
+	}
+
+	const closeTip = () => {
+		setTooltipShow(false);
 	}
 
 	useEffect(() => {
@@ -118,8 +124,8 @@ const Header = (props: any) => {
 					/>
 				)}
 
-				<nav className="bg-white relative flex flex-wrap items-center justify-between px-hg z-[1000]">
-					<div className="container px-0 lg:px-g flex flex-wrap lg:flex-nowrap items-center justify-between">
+				<nav className={`bg-white relative flex flex-wrap items-center justify-between px-hg z-[1000]`}>
+					<div className={`container px-0 lg:px-g flex flex-wrap lg:flex-nowrap items-center justify-between relative`}>
 						<button className="text-lg border-0 [flex-basis:30%] lg:hidden" type="button" data-cy="menu-icon" aria-label="Mobile navbar toggler" onClick={onToggleMobileNav}>
 							<span className="block w-[1.25em] h-[2px] bg-[#151515] relative before:-top-[.4em] before:w-[1.05em] before:h-[2px] before:bg-[#151515] before:absolute before:left-[0] after:content-[''] after:h-[2px] after:bg-body after:absolute after:left-[0] after:w-[.95em] after:top-[.4em]"></span>
 						</button>
@@ -190,9 +196,9 @@ const Header = (props: any) => {
 								</a>
 							</li>
 						</ul>
+						<Tooltip tooltipShow={tooltipShow} closeTip={closeTip} />
 					</div>
 				</nav>
-
 				<MobileMenu
 					onToggleMobileNav={onToggleMobileNav}
 					openDrawer={openDrawer}
