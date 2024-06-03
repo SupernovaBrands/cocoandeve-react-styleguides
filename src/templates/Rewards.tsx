86863d809c3377ef2c110067ab1bb30f96935329
Teddy Maulana <maulanateddy99@gmail.com>
2024-06-03 20:49:19 +0700

    general updates

---
+++ src/templates/Rewards.tsx
@@ -0,0 +1,12 @@
+import Script from "next/script";
+
+const Rewards = (props: any) => {
+    return (
+        <>
+            <Script type="text/javascript" src="https://cdn-widgetsrepository.yotpo.com/v1/loader/UDQ6dSW-H2Gi-71iOvYtnw" />
+            <div data-yotpo-instance-id="138067" className="yotpo-widget-instance"></div>
+        </>
+    )
+}
+
+export default Rewards;
