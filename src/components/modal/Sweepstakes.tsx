7eed13e9824100309a645284f0c1192a5160d685
Teddy Maulana <maulanateddy99@gmail.com>
2024-07-04 14:09:52 +0700

    update gaps

--- src/components/modal/Sweepstakes.tsx
+++ src/components/modal/Sweepstakes.tsx
@@ -188,13 +188,13 @@ const Sweepstakes: React.FC<SweepstakesProp> = ({ handleClose, data }) => {
 									{!emailError.valid && <span className='text-[#dc3545] text-xs block'>{emailError.error}</span>}
 									<input type="checkbox" name="tos" className="hidden" value="true" checked />
 									<div className="text-center mb-25 sweepstakes-popup__separator text-secondary">and / or</div>
-									<div className="relative flex items-stretch w-full flex-wrap">
+									<div className="relative flex items-stretch w-full flex-wrap mb-[5px]">
 										<InputCountry id="modal--sweepstakes__country" chevronCls="svg absolute fill-[#4e4e4e] h-[.75em] right-[.625em] top-[50%] [transform:translateY(-50%)]" className="bg-gray-400 py-[14px] px-[16px] rounded-h relative flex-[1_1_auto] w-[1%!important] bg-clip-padding" handleCode={handleCode} activeCountry={activeCountryCode} />
-										<input value={phone} onChange={handlePhone} className="bg-clip-padding block w-full mb-1 lg:mb-2 -ml-[1px] bg-gray-400 border-l-0 rounded-tl-none rounded-bl-none py-[14px] px-[16px] leading-[1.25] h-[3.125rem] rounded-h border border-gray-400 flex-[1_1_auto] w-[1%] lg:basis-[57.5%] sm:basis-[55%]" type="phone" placeholder={data?.sweepstakes_popup_phone} />
+										<input value={phone} onChange={handlePhone} className="bg-clip-padding block w-full mb-0 -ml-[1px] bg-gray-400 border-l-0 rounded-tl-none rounded-bl-none py-[14px] px-[16px] leading-[1.25] h-[3.125rem] rounded-h border border-gray-400 flex-[1_1_auto] w-[1%] lg:basis-[57.5%] sm:basis-[55%]" type="phone" placeholder={data?.sweepstakes_popup_phone} />
 									</div>
 									{!phoneError.valid && <span className='text-[#dc3545] text-xs block'>{phoneError.error}</span>}
-									<p className="hidden lg:block sweepstakes-popup__toc text-center mb-1 mt-0 font-size-sm text-body" dangerouslySetInnerHTML={{__html: data?.sweepstakes_foot_note.replace('text-underline', ' text-underline ') }}></p>
-									<Button  type="submit" buttonClass="btn-primary w-full border-0 py-g">Register now</Button>
+									<p className="hidden lg:block sweepstakes-popup__toc text-center mb-1 mt-1 mt-0 font-size-sm text-body" dangerouslySetInnerHTML={{__html: data?.sweepstakes_foot_note.replace('text-underline', ' text-underline ') }}></p>
+									<Button  type="submit" buttonClass="btn-primary w-full border-0 py-g mt-2 lg:mt-0">Register now</Button>
 									<p className="lg:hidden sweepstakes-popup__toc text-center mb-1 mt-1 font-size-xs text-body" dangerouslySetInnerHTML={{__html: data?.sweepstakes_foot_note.replace('text-underline', ' text-underline ') }}></p>
 								</form>
 							</div>
