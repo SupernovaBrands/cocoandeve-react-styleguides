3a85372ab6922f5a1fa41bb61f3417084867678a
Awais Gill <awais@supernovabrands.com>
2024-07-11 12:58:31 +0500

    Blog Template

--- src/templates/Blog.tsx
+++ src/templates/Blog.tsx
@@ -116,7 +116,7 @@ const Blog = (props) => {
 					<BlogNavTag href="/blogs/news/tagged/tan" title="Tan & SPF" active={active ? false : (tag === 'tan' ? true : false)}/>
 					<BlogNavTag href="/blogs/news/tagged/skin" title="Skin" active={active ? false : (tag === 'skin' ? true : false)}/>
 					<BlogNavTag href="/blogs/news/tagged/body" title="Body" active={active ? false : (tag === 'body' ? true : false)}/>
-					<a href="/blogs/news#how-to-tab" id="how-to-nav" onClick={handleHowTo} className={`me-1 mb-1 py-1 px-2 hover:no-underline sm:font-bold lg:font-normal lg:text-lg no-underline ${tag === 'all' && active === false ? 'active' : ''} ${active ? 'active' : ''}`}>How to's</a>
+					<a href="/blogs/news#how-to-tab" id="how-to-nav" onClick={handleHowTo} className={`me-1 mb-1 py-1 px-2 hover:no-underline sm:font-bold lg:font-normal lg:text-lg no-underline ${active ? 'active' : ''}`}>How to's</a>
 				</div>
 				{!activeFrame && (
 					<>
