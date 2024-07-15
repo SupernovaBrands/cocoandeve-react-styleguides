0fcfa1ba4d44a79b6d2aaa50c41afa136e1eae2d
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2024-07-15 18:29:20 +0700

    update post tag

--- src/components/PostTag.tsx
+++ src/components/PostTag.tsx
@@ -7,6 +7,14 @@ type PropType = {
 
 const PostTag: React.FC<PropType> = (props) => {
 	const { tag, children, paddingClass, widthClass } = props;
+	const handleTag = (e: any, tag: string) => {
+		const linkedTags = ['hair', 'tan', 'skin', 'body'];
+		let tagUrl = tag === 'suncare' ? 'tan' : tag;
+		if (linkedTags.includes(tagUrl)) {
+			e.preventDefault();
+			window.location.href = `/blogs/news/tagged/${tagUrl}`;
+		}
+	}
 	let className = 'bg-gray-400 text-gray-600 hover:text-gray-600';
 	if (tag === 'hair') className = 'bg-secondary-light text-gray-600 hover:text-gray-600 hover:bg-secondary-dark';
 	else if (tag === 'tan') className = 'bg-yellow-light text-gray-600 hover:text-gray-600 hover:bg-yellow-dark';
@@ -16,7 +24,7 @@ const PostTag: React.FC<PropType> = (props) => {
 	else if (tag === 'new') className = 'bg-secondary text-white hover:text-white';
 	else if (tag === 'hot') className = 'bg-primary text-white hover:text-white';
 	return (
-		<span className={`${paddingClass} inline-block badge no-underline hover:no-underline font-normal text-center mr-25 rounded ${className} ${widthClass}`}>{children}</span>
+		<span onClick={(e) => handleTag(e, tag)} className={`${paddingClass} cursor-pointer inline-block badge no-underline hover:no-underline font-normal text-center mr-25 rounded ${className} ${widthClass}`}>{children}</span>
 	)
 };
 
