eb94613d5569f5fc9da11e6ac2bdbe17ebfb851e
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2024-08-27 13:35:06 +0700

    shipping table pdp comp

--- src/components/modal/ShippingTable.tsx
+++ src/components/modal/ShippingTable.tsx
@@ -3,16 +3,44 @@ import CloseButton from '~/components/modal/CloseButton';
 const ShippingTable = (props: any) => {
 	const { handleClose, content, store, shippingTable } = props;
 	const shippingLineTable = content?.shippingLine?.[store];
-	const { shipping_table_heading, shipping_table_heading_title } = shippingTable;
+	const { shipping_table_heading, shipping_table_heading_title, shipping_table_title1, shipping_table_title2, shipping_table_title3 } = shippingTable;
+
+
+	const tableContent = shippingLineTable.shipping_popup_content?.split('::').map((tbl: any) => tbl.trim()) || [];
 	return (
 		<div className="bg-white flex items-center modal-content border border-[rgba(0,0,0,.2)] bg-clip-padding">
-			<div className="max-w-[31em] overflow-hidden relative mx-auto">
-				<CloseButton handleClose={handleClose} className="text-body" />
-				<div className="px-3 py-3 lg:px-4 lg:py-4">
-					<h2 dangerouslySetInnerHTML={{ __html: shipping_table_heading_title}} />
-					<p dangerouslySetInnerHTML={{ __html: shipping_table_heading}} />
-					<table className="shipping-table table table-bordered text-center mt-3 mb-2 aaa">
-						<tr><th>Country<br />(min spend)</th><th>Standard Shipping<br />(Orders over min spend)</th><th>Standard Shipping<br />(Orders under min spend)</th></tr><tr><td>France<br />(€60)</td><td>3 - 4 days <br />FREE</td><td>3 - 4 days <br />9.90€</td></tr><tr><td> Germany<br />(€60)</td><td>3 - 4 days <br />FREE</td><td>3 - 4 days <br />9.90€</td></tr><tr><td> Italy<br />(€60)</td><td>3 - 4 days <br />FREE</td><td>3 - 4 days <br />10.90€</td></tr><tr><td> Austria<br />(€60)</td><td>3 - 4 days <br />FREE</td><td>3 - 4 days <br />9.90€</td></tr><tr><td> Belgium<br />(€60)</td><td>3 - 4 days <br />FREE</td><td>3 - 4 days <br />9.90€</td></tr><tr><td> Bulgaria<br />(€100)</td><td>6 - 10 days <br />12.90€</td><td>6 - 10 days <br />12.90€</td></tr><tr><td> Croatia<br />(€60)</td><td>5 - 7 days <br />9.90€</td><td>5 - 7 days <br />9.90€</td></tr><tr><td> Czech <br />Republic<br />(€60)</td><td>4 - 5 days <br />FREE</td><td>4 - 5 days <br />10.90€</td></tr><tr><td> Denmark<br />(€100)</td><td>3 - 5 days <br />FREE</td><td>3 - 5 days <br />19.90€</td></tr><tr><td> Estonia<br />(€100)</td><td>5 - 7 days <br />12.90€</td><td>5 - 7 days <br />12.90€</td></tr><tr><td> Finland<br />(€100)</td><td>6 - 10 days <br />19.90€</td><td>6 - 10 days <br />19.90€</td></tr><tr><td> Greece<br />(€100)</td><td>5 - 7 days<br />12.90€</td><td>5 - 7 days <br />12.90€</td></tr><tr><td> Hungary<br />(€60)</td><td>3 - 4 days<br />FREE</td><td>3 - 4 days <br />9.90€</td></tr><tr><td> Ireland<br />(€60)</td><td>3 - 4 days<br />FREE</td><td>3 - 4 days <br />10.90€</td></tr><tr><td> Latvia<br />(€100)</td><td>5 - 7 days<br />FREE</td><td>5 - 7 days <br />12.90€</td></tr><tr><td> Luxembourg<br />(€60)</td><td>4 - 5 days<br />FREE</td><td>4 - 5 days <br />9.90€</td></tr><tr><td> Malta<br />(€60)</td><td>3 - 5 days<br />27.90€</td><td>3 - 5 days <br />27.90€</td></tr><tr><td> Monaco<br />(€60)</td><td>3 - 4 days<br />24.90€</td><td>3 - 4 days <br />24.90€</td></tr><tr><td> Netherlands <br />(€60)</td><td>2 - 3 days<br />FREE</td><td>2 - 3 days <br />9.90€</td></tr><tr><td> Poland<br />(€60)</td><td>3 - 4 days<br />FREE</td><td>3 - 4 days <br />10.90€</td></tr><tr><td> Portugal<br />(€60)</td><td>3 - 5 days<br />FREE</td><td>3 - 5 days <br />6.90€</td></tr><tr><td> Slovakia<br />(€60)</td><td>6 - 10 days<br />FREE</td><td>6 - 10 days <br />10.90€</td></tr><tr><td> Slovenia<br />(€60)</td><td>6 - 10 days<br />FREE</td><td>6 - 10 days <br />9.90€</td></tr><tr><td> Spain<br />(€60)</td><td>3 - 4 days<br />FREE</td><td>3 - 4 days <br />6.90€</td></tr><tr><td> Sweden<br />(€100)</td><td>5 - 7 days<br />19.90€</td><td>5 - 7 days <br />19.90€</td></tr></table>
+			<div className="overflow-hidden relative mx-auto">
+				{/* <CloseButton handleClose={handleClose} className="text-body" /> */}
+
+				<div className="px-3 py-g">
+					<button type="button" className="close float-right text-[1.5em] leading-[1]" onClick={handleClose}>
+						<span className="mb-0" aria-hidden="true">×</span>
+					</button>
+					{shipping_table_heading_title && (
+						<h2 className="mb-[.625rem]" dangerouslySetInnerHTML={{ __html: shipping_table_heading_title}} />
+					)}
+					{shipping_table_heading && (
+						<p className="mb-[1rem]" dangerouslySetInnerHTML={{ __html: shipping_table_heading}} />
+					)}
+
+					<table className="shipping-table table border border-gray-500 text-center mt-3 mb-2 aaa">
+						<tr>
+							<th className="p-[.5rem] border border-gray-500 align-top bg-secondary-light" dangerouslySetInnerHTML={{ __html: shipping_table_title1 }} />
+							<th className="p-[.5rem] border border-gray-500 align-top bg-secondary-light" dangerouslySetInnerHTML={{ __html: shipping_table_title2 }} />
+							<th className="p-[.5rem] border border-gray-500 align-top bg-secondary-light" dangerouslySetInnerHTML={{ __html: shipping_table_title3 }} />
+						</tr>
+						{/* {tableContent} */}
+						{tableContent && tableContent.map((row) => {
+							const cells = row.split(',');
+							return (
+								<tr>
+									{cells.map((cell) => (
+										<td className="p-[.5rem] border border-gray-500 align-top" dangerouslySetInnerHTML={{ __html: cell }} />
+									))}
+								</tr>
+							);
+						})}
+					</table>
 				</div>
 			</div>
         </div>
