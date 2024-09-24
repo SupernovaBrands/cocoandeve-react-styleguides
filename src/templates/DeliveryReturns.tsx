3fd66e9da62fba3da8096303cb1c32935fb02225
Awais Gill <awais@supernovabrands.com>
2024-09-25 04:14:21 +0500

    Display countries in shipping page in a table

--- src/templates/DeliveryReturns.tsx
+++ src/templates/DeliveryReturns.tsx
@@ -1,4 +1,39 @@
 import { useEffect, useState } from "react";
+import Austria from '~/images/countries/austria.svg';
+import Belgium from '~/images/countries/belgium.svg';
+import Bulgaria from '~/images/countries/bulgaria.svg';
+import Croatia from '~/images/countries/croatia.svg';
+import Czech from '~/images/countries/czech.svg';
+import Denmark from '~/images/countries/denmark.svg';
+import Estonia from '~/images/countries/estonia.svg';
+import Finland from '~/images/countries/finland.svg';
+import France from '~/images/countries/france.svg';
+import Germany from '~/images/countries/germany.svg';
+import Greece from '~/images/countries/greece.svg';
+import Hungary  from '~/images/countries/hungary.svg';
+import Ireland  from '~/images/countries/ireland.svg';
+import Italy from '~/images/countries/italy.svg';
+import Latvia from '~/images/countries/latvia.svg';
+import Luxembourg from '~/images/countries/luxembourg.svg';
+import Monaco from '~/images/countries/monaco.svg';
+import Netherlands from '~/images/countries/netherlands.svg';
+import Poland from '~/images/countries/poland.svg';
+import Portugal from '~/images/countries/portugal.svg';
+import Slovakia from '~/images/countries/slovakia.svg';
+import Slovenia from '~/images/countries/slovenia.svg';
+import Spain from '~/images/countries/spain.svg';
+import Sweden from '~/images/countries/sweden.svg';
+import Switzerland from '~/images/countries/switzerland.svg';
+import UnitedKingdom from '~/images/countries/uk.svg';
+import China from '~/images/countries/china.svg';
+import Macao from '~/images/countries/monaco.svg';
+import Malaysia from '~/images/countries/malaysia.svg';
+import Philippines from '~/images/countries/philippines.svg';
+import Singapore from '~/images/countries/singapore.svg';
+import Australia from '~/images/countries/australia.svg';
+import NewZealand from '~/images/countries/nz.svg';
+import Canada from '~/images/countries/canada.svg';
+import USA from '~/images/countries/usa.svg';
 
 const deliveryLi = (countryName) => {
     return (<li>{countryName}</li>);
@@ -8,6 +43,7 @@ const DeliveryReturns = (props: any) => {
     const { content } = props;
     const [pageContent, setPageContent] = useState(null);
     const [pageTitle, setPageTitle] = useState('Delivery');
+    const [isDesktop, setIsDesktop] = useState(false);
     const cols = [0, 1, 2, 3, 4, 5, 6];
 
     const getDataByIndex = (col) => {
@@ -32,6 +68,21 @@ const DeliveryReturns = (props: any) => {
             setPageTitle(content?.body?.[0]?.title);
         }
     }, [content]);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const handleResize = () => {
+                setIsDesktop(window.innerWidth >= 991);
+            };
+
+            handleResize();
+            window.addEventListener('resize', handleResize);
+
+            return () => {
+                window.removeEventListener('resize', handleResize);
+            };
+        }
+    }, []);
     return (
 		<>
             <section>
@@ -58,13 +109,111 @@ const DeliveryReturns = (props: any) => {
                     <div className=" lg:px-0 flex flex-wrap -mx-hg lg:-mx-g text-center justify-center pt-3 pb-[15px] lg:pb-2 border-b-[#adadad] border-b-[1px] border-b-solid">
                         <h2 className="block mb-2 lg:mb-[29px] w-full text-base">We deliver to the following countries:</h2>
                         <div className="flex flex-wrap justify-start lg:justify-center w-full">
-                            {pageContent?.delivery?.esc_countries_list && cols.map((col) => {
+                            {/* {pageContent?.delivery?.esc_countries_list && cols.map((col) => {
                                 if(getDataByIndex(col).length > 0) {
                                     return (<div className="w-1/2 lg:lg:w-[16.6%] px-hg lg:px-g pb-3">
                                         <ul className="delivery-countries-text m-0 text-left">{getDataByIndex(col)}</ul>
                                     </div>)
                                 }
-                            })}
+                            })} */}
+                            <div className="lg:w-2/5 w-full px-hg lg:px-g pb-3">
+                                <h5 className="font-bold mb-1 text-md text-left">Europe</h5>
+                                <div className="flex flex-wrap justify-start w-full">
+                                    {isDesktop ? (
+                                        <>
+                                            <ul className="w-[30%] delivery-countries-text m-0 text-left text-sm lg:block hidden">
+                                                <li className="flex"><Austria className="mr-[7px] mt-[2px]" />Austria</li>
+                                                <li className="flex"><Belgium className="mr-[7px] mt-[2px]" />Belgium</li>
+                                                <li className="flex"><Bulgaria className="mr-[7px] mt-[2px]" />Bulgaria</li>
+                                                <li className="flex"><Croatia className="mr-[7px] mt-[2px]" />Croatia</li>
+                                                <li className="flex"><Czech className="mr-[7px] mt-[2px]" />Czech Republic</li>
+                                                <li className="flex"><Denmark className="mr-[7px] mt-[2px]" />Denmark</li>
+                                                <li className="flex"><Estonia className="mr-[7px] mt-[2px]" />Estonia</li>
+                                                <li className="flex"><Finland className="mr-[7px] mt-[2px]" />Finland</li>
+                                                <li className="flex"><France className="mr-[7px] mt-[2px]" />France</li>
+                                                <li className="flex"><Germany className="mr-[7px] mt-[2px]" />Germany</li>
+                                            </ul>
+                                            <ul className="w-[30%] delivery-countries-text m-0 text-left text-sm lg:block hidden">
+                                                <li className="flex"><Greece className="mr-[7px] mt-[2px]" />Greece</li>
+                                                <li className="flex"><Hungary className="mr-[7px] mt-[2px]" />Hungary</li>
+                                                <li className="flex"><Ireland className="mr-[7px] mt-[2px]" />Ireland</li>
+                                                <li className="flex"><Italy className="mr-[7px] mt-[2px]" />Italy</li>
+                                                <li className="flex"><Latvia className="mr-[7px] mt-[2px]" />Latvia</li>
+                                                <li className="flex"><Luxembourg className="mr-[7px] mt-[2px]" />Luxembourg</li>
+                                                <li className="flex"><Monaco className="mr-[7px] mt-[2px]" />Monaco</li>
+                                                <li className="flex"><Netherlands className="mr-[7px] mt-[2px]" />Netherlands</li>
+                                                <li className="flex"><Poland className="mr-[7px] mt-[2px]" />Poland</li>
+                                            </ul>
+                                            <ul className="w-[30%] delivery-countries-text m-0 text-left text-sm lg:block hidden">
+                                                <li className="flex"><Portugal className="mr-[7px] mt-[2px]" />Portugal</li>
+                                                <li className="flex"><Slovakia className="mr-[7px] mt-[2px]" />Slovakia</li>
+                                                <li className="flex"><Slovenia className="mr-[7px] mt-[2px]" />Slovenia</li>
+                                                <li className="flex"><Spain className="mr-[7px] mt-[2px]" />Spain</li>
+                                                <li className="flex"><Sweden className="mr-[7px] mt-[2px]" />Sweden</li>
+                                                <li className="flex"><Switzerland className="mr-[7px] mt-[2px]" />Switzerland</li>
+                                                <li className="flex"><UnitedKingdom className="mr-[7px] mt-[2px]" />United Kingdom</li>
+                                            </ul>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <ul className="w-1/2 delivery-countries-text m-0 text-left text-sm lg:hidden block">
+                                                <li className="flex"><Austria className="mr-[7px] mt-[2px]" />Austria</li>
+                                                <li className="flex"><Belgium className="mr-[7px] mt-[2px]" />Belgium</li>
+                                                <li className="flex"><Bulgaria className="mr-[7px] mt-[2px]" />Bulgaria</li>
+                                                <li className="flex"><Croatia className="mr-[7px] mt-[2px]" />Croatia</li>
+                                                <li className="flex"><Czech className="mr-[7px] mt-[2px]" />Czech Republic</li>
+                                                <li className="flex"><Denmark className="mr-[7px] mt-[2px]" />Denmark</li>
+                                                <li className="flex"><Estonia className="mr-[7px] mt-[2px]" />Estonia</li>
+                                                <li className="flex"><Finland className="mr-[7px] mt-[2px]" />Finland</li>
+                                                <li className="flex"><France className="mr-[7px] mt-[2px]" />France</li>
+                                                <li className="flex"><Germany className="mr-[7px] mt-[2px]" />Germany</li>
+                                                <li className="flex"><Greece className="mr-[7px] mt-[2px]" />Greece</li>
+                                                <li className="flex"><Hungary className="mr-[7px] mt-[2px]" />Hungary</li>
+                                                <li className="flex"><Ireland className="mr-[7px] mt-[2px]" />Ireland</li>
+                                            </ul>
+                                            <ul className="w-1/2 delivery-countries-text m-0 text-left text-sm lg:hidden block">
+                                            <li className="flex"><Italy className="mr-[7px] mt-[2px]" />Italy</li>
+                                                <li className="flex"><Latvia className="mr-[7px] mt-[2px]" />Latvia</li>
+                                                <li className="flex"><Luxembourg className="mr-[7px] mt-[2px]" />Luxembourg</li>
+                                                <li className="flex"><Monaco className="mr-[7px] mt-[2px]" />Monaco</li>
+                                                <li className="flex"><Netherlands className="mr-[7px] mt-[2px]" />Netherlands</li>
+                                                <li className="flex"><Poland className="mr-[7px] mt-[2px]" />Poland</li>
+                                                <li className="flex"><Portugal className="mr-[7px] mt-[2px]" />Portugal</li>
+                                                <li className="flex"><Slovakia className="mr-[7px] mt-[2px]" />Slovakia</li>
+                                                <li className="flex"><Slovenia className="mr-[7px] mt-[2px]" />Slovenia</li>
+                                                <li className="flex"><Spain className="mr-[7px] mt-[2px]" />Spain</li>
+                                                <li className="flex"><Sweden className="mr-[7px] mt-[2px]" />Sweden</li>
+                                                <li className="flex"><Switzerland className="mr-[7px] mt-[2px]" />Switzerland</li>
+                                                <li className="flex"><UnitedKingdom className="mr-[7px] mt-[2px]" />United Kingdom</li>
+                                            </ul>
+                                        </>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="lg:w-1/5 sm:w-1/2 px-hg lg:px-g pb-3">
+                                <h5 className="font-bold mb-1 text-md text-left">Asia</h5>
+                                <ul className="delivery-countries-text m-0 text-left text-sm">
+                                    <li className="flex"><China className="mr-[7px] mt-[2px]" />China <br className="block lg:hidden"/>(Hong Kong, Macao)</li>
+                                    <li className="flex"><Malaysia className="mr-[7px] mt-[2px]" />Malaysia</li>
+                                    <li className="flex"><Philippines className="mr-[7px] mt-[2px]" />Philippines</li>
+                                    <li className="flex"><Singapore className="mr-[7px] mt-[2px]" />Singapore</li>
+
+                                </ul>
+                            </div>
+                            <div className="lg:w-1/5 sm:w-1/2 px-hg lg:px-g pb-3">
+                                <h5 className="font-bold mb-1 text-md text-left">Oceania</h5>
+                                <ul className="delivery-countries-text m-0 text-left text-sm">
+                                    <li className="flex"><Australia className="mr-[7px] mt-[2px]" />Australia</li>
+                                    <li className="flex"><NewZealand className="mr-[7px] mt-[2px]" />New Zealand</li>
+                                </ul>
+                            </div>
+                            <div className="lg:w-1/5 w-full px-hg lg:px-g pb-3">
+                                <h5 className="font-bold mb-1 text-md text-left">North America</h5>
+                                <ul className="delivery-countries-text m-0 text-left text-sm">
+                                    <li className="flex"><Canada className="mr-[7px] mt-[2px]" />Canada</li>
+                                    <li className="flex"><USA className="mr-[7px] mt-[2px]" />United States</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                     <div className="flex flex-col -mx-hg lg:-mx-g text-center justify-center pb-4 pt-3">
