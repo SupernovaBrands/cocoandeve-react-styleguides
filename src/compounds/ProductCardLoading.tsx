be0ee38f415a1618accd90b9bdbd048c656b54a0
Teddy Maulana <maulanateddy99@gmail.com>
2024-09-26 20:07:02 +0700

    update card loading

--- src/compounds/ProductCardLoading.tsx
+++ src/compounds/ProductCardLoading.tsx
@@ -1,7 +1,7 @@
 const ProductCardLoading = (props) => {
     return (
         <div key={props.keyName} className={`${props.className} lg:hidden`}>
-            <div className="bg-shimmer pt-[150%] rounded"></div>
+            <div className="bg-shimmer pt-[190%] rounded"></div>
         </div>
     )
 }
