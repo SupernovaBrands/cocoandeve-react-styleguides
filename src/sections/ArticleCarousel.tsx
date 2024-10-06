ef7c6055d772bb88e73c537084ca24bf96ff2f03
Awais Gill <awais@supernovabrands.com>
2024-10-07 01:42:38 +0500

    Show a hint of next blog article on carousel

--- src/sections/ArticleCarousel.tsx
+++ src/sections/ArticleCarousel.tsx
@@ -54,8 +54,8 @@ const ArticleCarousel = (props:any) => {
 			<Carousel.Wrapper emblaApi={emblaApi} className="mb-1">
 				<Carousel.Inner emblaRef={emblaRef} className="-mx-hg lg:-mx-g">
 					{articles.map((data:any, index:number) => (
-						<PostCard key={`${data.id}-${index}`} carousel={true} template="pdp" className="flex-grow-0 flex-shrink-0 w-full basis-full px-hg lg:px-g lg:w-1/2 lg:basis-1/2" data={data} />
-						))}
+						<PostCard key={`${data.id}-${index}`} carousel={true} template="pdp" className="flex-grow-0 flex-shrink-0 basis-[91.5%] w-[91.5%] px-hg lg:px-g lg:w-1/2 lg:basis-1/2" data={data} />
+					))}
 				</Carousel.Inner>
 				<Carousel.Navigation>
 					<PrevButton
