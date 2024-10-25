eacb6790b151165bbced5cd5353b74a22a6875c4
Awais Gill <awais@supernovabrands.com>
2024-10-25 15:24:44 +0500

    Make announcement bar sticky

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -260,7 +260,7 @@ const Header = (props: any) => {
 									<Account className={`hover:fill-primary text-[1.375em] h-[1em] mr-[5px] ${openAccountBox ? 'fill-primary' : ''}`} />
 								</button>
 								{!isLoggedIn && (
-									<AccountDropdown store={store} timerData={timerBar} annBarEnabled={annBar?.enabled} scrolled={scrolled} swellLoyalty={swellLoyalty} openAccountBox={openAccountBox} toggleAccountDropdown={toggleAccountDropdown} />
+									<AccountDropdown store={store} timerData={timerBar} annBarEnabled={annBar?.enabled} scrolled={scrolled} swellLoyalty={swellLoyalty} openAccountBox={openAccountBox} isStickyEnabled={annBar?.isStickyEnabled} isScrollEnabled={annBar?.isScrollEnabled} toggleAccountDropdown={toggleAccountDropdown} />
 								)}
 							</li>
 							<li key="search" className="nav-item pr-g lg:pl-hg">
