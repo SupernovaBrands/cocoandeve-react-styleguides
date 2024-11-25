ceed31c42fb5baafe8a9ffb4d65afaf9f3c0e206
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2024-11-25 15:12:37 +0700

    86equmnu5 Hardcode Sets containing LAV Shampoo & Conditioner in CA

--- src/compounds/SearchProductCard.tsx
+++ src/compounds/SearchProductCard.tsx
@@ -4,16 +4,16 @@ const SearchProductCard = (props: any) => {
 	const  { url, title, img, trackEvent, store } = props;
 	let featuredImageUrl = img;
 
-	if ((store === 'my' || store === 'uk' || store === 'int' || store === 'au' || store === 'eu') && url === 'daily-essentials-bundle') {
+	if ((store === 'my' || store === 'uk' || store === 'int' || store === 'au' || store === 'eu' || store === 'ca') && url === 'daily-essentials-bundle') {
 		featuredImageUrl = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/503a39e9-27b7-4278-c850-1d015cb06000/public';
 	}
-	if ((store === 'my' || store === 'uk' || store === 'int') && url === 'super-hydration-kit') {
+	if ((store === 'my' || store === 'uk' || store === 'int' || store === 'ca') && url === 'super-hydration-kit') {
 		featuredImageUrl = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/764f5eec-ddf2-4ead-e1c6-88a0ab395200/public';
 	}
 	if ((store === 'dev' || store === 'au' || store === 'eu') && url === 'super-hydration-kit') {
 		featuredImageUrl = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/ee76b291-fb34-4025-4476-e0d564560c00/public';
 	}
-	if ((store === 'my' || store === 'uk' || store === 'int' || store === 'au' || store === 'eu') && url === 'deep-condition-bundle') {
+	if ((store === 'my' || store === 'uk' || store === 'int' || store === 'au' || store === 'eu' || store === 'ca') && url === 'deep-condition-bundle') {
 		featuredImageUrl = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/10045f09-b506-4fc1-c28a-b1ced8673800/public';
 	}
 	if (store === 'my' && url === 'hair-revival-set') {
