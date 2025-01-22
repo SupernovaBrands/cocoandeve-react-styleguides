c5a4740bc09606fd59947a5acda29e533d0a6fcf
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2025-01-22 14:16:17 +0700

    86er9r408 about us page smaller

--- src/compounds/ImageWithText.tsx
+++ src/compounds/ImageWithText.tsx
@@ -9,7 +9,7 @@ interface ImageWithTextProps {
 
 const ImageWithText: React.FC<ImageWithTextProps> = (props) => {
     return (
-        <figure className={`flex flex-wrap -mx-hg lg:-mx-g relative items-center mb-[1rem] lg:mb-2 mt-5 ${props.reverse ? 'flex-row-reverse' : ''}`}>
+        <figure className={`flex flex-wrap -mx-hg lg:-mx-g relative items-center mb-0 lg:mb-2 mt-3 px-g ${props.reverse ? 'flex-row-reverse' : ''} lg:mb-[50px] lg:mt-[50px]`}>
             <div className="-z-[1] lg:order-last w-full lg:w-2/3 lg:px-g">
                 {props.src && (
                     <div className="sm:mx-0 lg:mx-0">
@@ -21,7 +21,7 @@ const ImageWithText: React.FC<ImageWithTextProps> = (props) => {
                 )}
             </div>
             <figcaption className="w-full lg:w-1/3 px-g lg:px-g">
-                <div className={`${!props.reverse ? 'lg:-mr-[40%]' : ''} sm:-mt-[7em] text-start lg:mt-0 p-2 lg:pt-3 lg:pb-3 lg:pr-3 bg-white ${props.reverse ? 'lg:-ml-[40%] mr-auto lg:pl-3' : ''}`}>
+                <div className={`${!props.reverse ? 'lg:-mr-[58.5%]' : ''} sm:-mt-[7em] text-start lg:mt-0 pr-g pt-2 pb-1 lg:pt-3 lg:pb-3 bg-white ${props.reverse ? 'lg:-ml-[58.5%] mr-auto lg:pl-3 lg:pr-0' : 'lg:pr-3'}`}>
                     {props.children}
                 </div>
             </figcaption>
