d3f62ed68caa0aec6c765cdabc87da001e039cd9
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2025-01-28 15:12:12 +0700

    86er9r408 page ourstory update

--- src/compounds/ImageWithText.tsx
+++ src/compounds/ImageWithText.tsx
@@ -9,19 +9,19 @@ interface ImageWithTextProps {
 
 const ImageWithText: React.FC<ImageWithTextProps> = (props) => {
     return (
-        <figure className={`flex flex-wrap -mx-hg lg:-mx-g relative items-center mb-0 lg:mb-2 mt-3 px-g ${props.reverse ? 'flex-row-reverse' : ''} lg:mb-[50px] lg:mt-[50px]`}>
-            <div className="-z-[1] lg:order-last w-full lg:w-2/3 lg:px-g">
+        <figure className={`flex flex-wrap -mx-hg lg:-mx-g relative items-center mb-0 lg:mb-2 mt-4 px-g lg:max-w-[92.5%] lg:mx-auto ${props.reverse ? 'flex-row-reverse' : ''} lg:mb-[50px] lg:mt-[50px]`}>
+            <div className={`-z-[1] lg:order-last w-full lg:w-[570px] lg:basis-[570px] lg:px-g ${props.reverse ? 'lg:h-[274px]' : 'lg:h-[264px]'}`}>
                 {props.src && (
                     <div className="sm:mx-0 lg:mx-0">
                         <picture>
                             {props.srcSet && <source srcSet={props.srcSet} media="(min-width: 992px)" />}
-                            <img src={props.src} className="w-full" alt="" />
+                            <img src={props.src} className={`w-full ${props.reverse ? 'lg:h-[274px]' : 'lg:h-[264px]'}`} alt="" />
                         </picture>
                     </div>
                 )}
             </div>
-            <figcaption className="w-full lg:w-1/3 px-g lg:px-g">
-                <div className={`${!props.reverse ? 'lg:-mr-[58.5%]' : ''} sm:-mt-[7em] text-start lg:mt-0 px-g pt-2 pb-1 lg:pt-3 lg:pb-3 bg-white ${props.reverse ? 'lg:-ml-[58.5%] mr-auto lg:pl-3 lg:pr-0' : 'lg:pr-3'}`}>
+            <figcaption className="w-full lg:w-[calc(100%-570px)] lg:basis-[calc(100%-570px)] px-g lg:px-g">
+                <div className={`${props.reverse ? 'lg:-ml-[20.5%] mr-auto lg:pl-3 lg:pr-0' : 'lg:-mr-[25%] lg:pr-3'} sm:-mt-[4.5em] text-start lg:mt-0 px-g pt-2 pb-1 lg:pt-3 lg:pb-3 bg-white`}>
                     {props.children}
                 </div>
             </figcaption>
--- src/templates/OurStory.tsx
+++ src/templates/OurStory.tsx
@@ -6,7 +6,7 @@ import Play from '~/images/icons/play.svg';
 import Close from '~/images/icons/close.svg';
 import ChevronNext from '~/images/icons/chevron-next.svg';
 import ChevronPrev from '~/images/icons/chevron-prev.svg';
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import Carousel from '~/components/carousel/EmblaCarouselMulti';
 import {
 	PrevButton,
@@ -56,14 +56,18 @@ const OurStoryTemplate = (props: any) => {
 	const reorderLogos = [...restLogos,firstLogo];
 	const logos = [...reorderLogos, ...reorderLogos];
 
+	useEffect(() => {
+		document.body.classList.add('p3-site-smaller');
+	}, []);
+
 	return (
 		<>
 			<section className="page-banner-image flex relative justify-center items-center p-0 w-full">
 				<h1 className="text-center absolute mx-g my-1 text-xl lg:text-[3.375em] lg:leading-[1] text-white lg:mx-[10%]">{banner.tiitle}</h1>
 				{!isLoading && (
 					<picture>
-						<source srcSet={banner.back_img.url} media="(min-width: 992px)" />
-						<img src={banner.back_img_mob.url} className="w-full" alt={banner.tiitle} />
+						<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/a1dca008-8438-4aaf-d8ba-ad72d7668500/public" media="(min-width: 992px)" />
+						<img src="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/6d1e9527-c4f1-42dd-4229-ac48de60a500/828x" className="w-full" alt={banner.tiitle} />
 					</picture>
 				)}
 			</section>
@@ -76,18 +80,18 @@ const OurStoryTemplate = (props: any) => {
 					}} />
 				)}
 				<ImageWithText
-					src={!isLoading ? intro.back_img_mob_1.url : null}
-					srcSet={intro.back_img_1.url}>
+					src={!isLoading ? 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/342a6ea4-56aa-4b59-75b5-0bab0cd19700/public' : null}
+					srcSet={'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/d2fefbbc-8dcb-4782-db07-ffa713901500/public'}>
 					<h2 className="mb-[.5rem] text-lg text-body lg:text-xl">{intro.titleintro_1}</h2>
 					{!isLoading && <p className="text-body">{intro.description_1}</p>}
 				</ImageWithText>
 
 				<ImageWithText
 					reverse={true}
-					src={!isLoading ? intro.back_img_mob_2.url : null}
-					srcSet={intro.back_img_2.url}>
+					src={!isLoading ? 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/ad271208-07c1-4920-b861-9de497293a00/public' : null}
+					srcSet={'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/8cff9e32-7b24-4fd7-793f-fdea4fc2e600/public'}>
 					<h2 className="mb-[.5rem] text-lg text-body lg:text-xl">{intro.titleintro_2}</h2>
-					{!isLoading && <p className="mb-[1rem] text-body">{intro.description_2}</p>}
+					{!isLoading && <p className="mb-[1rem] lg:mb-0 text-body">{intro.description_2}</p>}
 				</ImageWithText>
 			</section>
 
@@ -139,17 +143,17 @@ const OurStoryTemplate = (props: any) => {
 			<section className="bg-pink-light w-full justify-center lg:flex items-center p-0">
 				<div className="container px-hg lg:absolute">
 					<div className="flex flex-wrap mobile-wrapper -mx-hg lg:-mx-g px-hg">
-						<div className="px-hg lg:px-g lg:w-1/3 pt-[40px] pb-[40px]">
+						<div className="px-hg lg:pl-3 lg:pr-0 lg:w-1/3 pt-[40px] pb-[40px]">
 							<h4 className="mb-1 font-normal lg:text-lg">{videoBanner.title}</h4>
 							<h2 className="mb-0 lg:text-2xl">{videoBanner.description}</h2>
 						</div>
 					</div>
 				</div>
 				{!isLoading && (
-					<figure className="ml-auto lg:w-7/12 lg:basis-7/12 grow-0 p-0 relative mb-0">
+					<figure className="ml-auto lg:w-[718px] lg:basis-[718px] grow-0 p-0 relative mb-0">
 						<picture className={`relative block cursor-pointer lg:z-[2]`} onClick={() => handlOpenModal(true)}>
-							<source srcSet={videoBanner.back_img.url} media="(min-width: 992px)" />
-							<img src={videoBanner.back_img_mob.url} className="w-full rounded-0" alt="Behind the scenes" />
+							<source srcSet={'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/191aae59-800a-4906-b776-b94aded44d00/public'} media="(min-width: 992px)" />
+							<img src={'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/2db1b4fd-acbe-45c4-e5b3-a68312dcd700/public'} className="w-full rounded-0" alt="Behind the scenes" />
 							<div className="flex items-center absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
 								<Play className="svg--current-color w-[57px] h-[57px] lg:w-[88px] lg:h-[88px] text-white " />
 								{/* <span className="text-white font-bold text-[1.625em] lg:text-[2.75em] ml-1 lg:ml-g">Play Video</span> */}
