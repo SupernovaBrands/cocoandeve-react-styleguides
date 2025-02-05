458eec4b41a47a8a1e6a9c8e22d52d92bf93637b
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2025-02-05 10:34:14 +0700

    860t5d37a update blog & article layout

--- src/compounds/PostCard.tsx
+++ src/compounds/PostCard.tsx
@@ -14,13 +14,14 @@ type PropType = {
 	height?: string
 	pictureClass?: string
 	carousel?: boolean
+	showSubtext?: boolean
 }
 
 const PostCard: React.FC<PropType> = (props) => {
-	const { className, data, template, imgClass, textPrimary, textClass, height, pictureClass } = props;
+	const { className, data, template, imgClass, textPrimary, textClass, height, pictureClass, showSubtext } = props;
 	return (
 		<article className={`${className}`}>
-			<figure className={`${template === 'blog' || template === 'pdp' ? 'border border-secondary-light' : 'post-card mb-4 lg:mb-0 bg-white h-full'} ${template === 'article' ? 'lg:mx-0 sm:-mx-g !h-auto lg:!h-full !mb-0' : ''} h-auto lg:h-full flex flex-col ${height} rounded`}>
+			<figure className={`border border-secondary-light ${template === 'blog' || template === 'pdp' ? '' : 'post-card mb-4 lg:mb-0 bg-white h-full'} ${template === 'article' ? 'lg:mx-0 sm:-mx-g !h-auto lg:!h-full !mb-0' : ''} h-auto lg:h-full flex flex-col ${height} rounded`}>
 				{data.img && (
 					<a href={data.handle ?? '#'} className={`relative block hover:after:bg-white hover:after:bg-opacity-20 after:content-[''] after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:transition-colors after:duration-150 after:ease-in-out`} aria-label={data.title}>
 						<picture className={`${template === 'article' || imgClass ? 'embed-responsive' : ''} ${pictureClass}`}>
@@ -40,17 +41,19 @@ const PostCard: React.FC<PropType> = (props) => {
 								</>
 							)}
 						</div>
-						<p className={`font-[400] text-lg ${template === 'blog' ? 'blog-post__card' : ''} ${template === 'article' ? 'hover:text-primary hover:underline' : 'hover:no-underline'} mt-2 leading-[1.25!important] mb-1 block text-body hover:text-body`}>{data.title}</p>
-						{/* <div className={`group-hover:text-primary-darken post-card__description ${template === 'article' ? 'text-body hover:text-body mt-0 px-3 mb-0 pb-1' : ''} ${textClass} ${template === 'blog' ? 'blog-card__desc mb-[1rem]' : 'sm:grow-0 lg:flex-grow'} ${textPrimary ? 'text-primary hover:text-primary-darken hover:no-underline' : ''} flex flex-col ${template === 'pdp' ? 'mb-[32px] lg:mb-2' : ''}`}>
-							{ template === 'pdp' && data.description && (
-								parse(data.description)
-							)}
-							{template !== 'pdp' && data.description && (
-								parse(data.description.replace(/<[^>]*>?/gm, ''))
-							)}
-						</div> */}
+						<p className={`font-bold text-lg ${template === 'blog' ? 'blog-post__card' : ''} ${template === 'article' ? 'hover:text-primary hover:underline' : 'hover:no-underline'} mt-2 leading-[1.25!important] ${showSubtext ? 'mb-1 lg:mb-[1rem]' : 'mb-[1rem]'} block text-body hover:text-body`}>{data.title}</p>
+						{showSubtext && (
+							<div className={`group-hover:text-primary-darken post-card__description ${template === 'article' ? 'text-body hover:text-body mt-0 px-3 mb-0 pb-1' : ''} ${textClass} ${template === 'blog' ? 'blog-card__desc mb-[1rem]' : `sm:grow-0 ${showSubtext ? '' : 'lg:flex-grow'}`} ${textPrimary ? 'text-primary hover:text-primary-darken hover:no-underline' : 'text-body'} flex flex-col ${template === 'pdp' ? 'mb-[32px] lg:mb-2' : ''}`}>
+								{ template === 'pdp' && data.description && (
+									parse(data.description)
+								)}
+								{template !== 'pdp' && data.description && (
+									parse(data.description.replace(/<[^>]*>?/gm, ''))
+								)}
+							</div>
+						)}
 						{template === 'blog' || template === 'pdp' || template === 'article' ? (
-							<span className="btn btn-outline-primary self-start hover:no-underline leading-[1.25!important] mt-0 lg:mt-auto mb-0 flex rounded" aria-label={data.title}>Read more</span>
+							<span className={`btn btn-outline-primary self-start hover:no-underline leading-[1.25!important] mt-0 mb-0 flex rounded ${showSubtext ? 'lg:py-[14px] lg:px-[53px]' : 'lg:mt-auto'}`} aria-label={data.title}>Read more</span>
 						) : (
 							<span className="inline-block px-3 pb-2 block no-underline hover:underline leading-[1.25!important] rounded" aria-label={data.title}>Read more <ChevronNext className="svg inline-block font-size-xs fill-primary" /></span>
 						)}
