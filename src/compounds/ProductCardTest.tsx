d3e860eeb6c29a78c8064ebb80148cfe428f3a52
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2025-02-20 11:10:29 +0700

    sync master

--- src/compounds/ProductCardTest.tsx
+++ src/compounds/ProductCardTest.tsx
@@ -144,6 +144,12 @@ const SwatchOverlay = (props:any) => {
     if (firstAvailable === null || !firstAvailable?.availableForSale) {
         firstAvailable = props.swatch.data.find((swatchData:any) => swatchData.available) || { id: 0 };
     }
+    if (product.handle === 'bronzing-self-tanner-drops' && ['dev', 'us'].includes(store)) {
+        let swatch = props.swatch.data.find((swatchData:any) => swatchData.value === 'medium');
+        if (swatch.availableForSale) {
+            firstAvailable = swatch;
+        }
+    }
     const [selectedVariant, setSelectedVariant] = useState(firstAvailable || null);
 
     const changeSwatch = (e:any) => {
@@ -301,7 +307,7 @@ const ProductCardTest = (props:any) => {
         }
 
         // https://app.clickup.com/t/86ergy8je
-        if (product?.variants?.nodes.length > 1 && selectedVariant?.availableForSale && !product?.variants?.nodes[1].availableForSale) {
+        if (!product.availableForSale && selectedVariant?.availableForSale) {
             selectedVariant.availableForSale = false;
         }
     }, [product, selectedVariant]);
@@ -316,19 +322,6 @@ const ProductCardTest = (props:any) => {
         if (defaultVariant === null && !product.swatch) defaultVariant = variantNodes.sort((x, y) => y.availableForSale - x.availableForSale)[0];
         setSelectedVariant(defaultVariant || null);
     }, []);
-
-    // useEffect(() => {
-    //     if (product.handle === 'bronzing-self-tanner-drops' && ['dev', 'us'].includes(store)) {
-    //         let firstAvailable: any;
-    //         if (autoTicks && autoTicks.length > 0) {
-    //             firstAvailable = product?.variants?.nodes.find((obj) => (autoTicks.includes(parseInt(obj.id.replace('gid://shopify/ProductVariant/', ''))))) || null;
-    //         }
-    //         if (firstAvailable === null || !firstAvailable?.availableForSale) {
-    //             firstAvailable = product.swatch.data.find((swatchData:any) => swatchData.available) || { id: 0 };
-    //         }
-    //         setShade(firstAvailable.value)
-    //     }
-    // }, [product]);
 
     useEffect(() => {
         if (product.handle === 'bronzing-self-tanner-drops' && ['dev', 'us'].includes(store)) {
