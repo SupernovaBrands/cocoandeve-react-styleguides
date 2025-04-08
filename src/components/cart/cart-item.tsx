99e3adbc3354655ffe9a6b3c6fb5c3b37e7b772b
Awais Gill <awais@supernovabrands.com>
2025-04-08 09:25:15 +0500

    Hardcode: Bronzing Face Drops in AU

--- src/components/cart/cart-item.tsx
+++ src/components/cart/cart-item.tsx
@@ -179,12 +179,20 @@ export const CartItem = (props:CartItemProps) => {
 			// 	featuredImage = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/a5d370c5-14d7-4c95-179b-4a8695738100/public';
 			// }
 
-			// if (['au'].includes(store) && item.merchandise.product.handle === 'bronzing-self-tanner-drops' && selectedVariant[0].toLowerCase() === 'medium') {
-			// 	featuredImage = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/f7288c0a-349c-4253-9723-6a17973e5900/public';
-			// }
+			if (['au'].includes(store) && item.merchandise.product.handle === 'bronzing-self-tanner-drops' && selectedVariant[0].toLowerCase() === 'medium') {
+				featuredImage = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/e9f74cf3-1826-41a6-dde2-70b4fd315100/public';
+			}
 
 			if (['au'].includes(store) && item.merchandise.product.handle === 'bronzing-self-tanner-drops' && selectedVariant[0].toLowerCase() === 'dark') {
 				featuredImage = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/11f0acb0-5f8a-459e-9d31-6f706061df00/public';
+			}
+
+			if (['au'].includes(store) && item.merchandise.product.handle === 'all-in-glow-set' && selectedVariant[0].toLowerCase() === 'medium') {
+				featuredImage = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/f44857b0-8dfe-4373-4abc-7087f47ecb00/public';
+			}
+
+			if (['au'].includes(store) && item.merchandise.product.handle === 'all-in-glow-set' && selectedVariant[0].toLowerCase() === 'dark') {
+				featuredImage = 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/1c91fe5f-e66d-4efa-13b9-6dbb4b33c100/public';
 			}
 		}
 
