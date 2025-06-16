eac5eabe5f5ac133214a7d5f1c595142f5bafc6c
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2025-06-16 20:56:57 +0700

    86etcahzw sidebar

---
+++ src/compounds/YourBundleSidebar.tsx
@@ -0,0 +1,112 @@
+import PercentageSmall from '~/images/icons/percentage-small.svg';
+import DeliverySmall from '~/images/icons/delivery-small.svg';
+import ChevronUp from '~/images/icons/chevron-up.svg';
+import { formatMoney } from "~/modules/utils";
+import { Button } from "../components";
+import { useEffect, useState } from 'react';
+
+const ItemCard = (props) => {
+    const { item, placeholder, store, index } = props;
+    return (
+        <li className={`flex mb-[1rem] ${placeholder ? 'opacity-[.5]' : ''}`}>
+            <span className="inline-block font-bold mr-[.5rem] mt-[.5rem]">{index + 1}</span>
+            <figure className="flex rounded-[1rem] bg-white p-[.5rem] w-full">
+                {placeholder && <div className="w-[3rem] h-[3rem] rounded-[.5rem] bg-gray-400" />}
+                {!placeholder && <img src={item.src} width={48} height={48} className="w-[3rem] h-[3rem] rounded-[.5rem]" />}
+                <figcaption className="ml-[.5rem]">
+                    <h4 className="font-bold text-sm leading-[normal] mb-[.25rem] mr-25">{item.title}</h4>
+                    {!placeholder && (
+                        <>
+                            <div className="flex mb-[.25rem]">
+                                {item.comparePrice && <span className="line-through mr-1">{formatMoney(item.comparePrice, false, store)}</span>}
+                                <span className="font-bold">{formatMoney(item.price, false, store)}</span>
+                            </div>
+                            <span className="text-underline text-primary font-bold cursor-pointer">Edit</span>
+                        </>
+                    )}
+                </figcaption>
+            </figure>
+        </li>
+    )
+};
+
+const YourBundleSidebar = (props: any) => {
+    const { bundleSize, bundleDiscount, itemSelected, store } = props;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [selected, setSelected] = useState(itemSelected);
+
+    useEffect(() => {
+        if (isOpen) document.body.classList.add('!overflow-y-hidden', 'lg:!overflow-y-scroll')
+        else document.body.classList.remove('!overflow-y-hidden', 'lg:!overflow-y-scroll')
+    }, [isOpen]);
+
+    useEffect(() => {
+        // if (itemSelected.length > 0) {
+        const selectProducts = [];
+        let counter = 1;
+        [...Array(bundleSize)].forEach(() => {
+            selectProducts.push({
+                idx: counter++,
+                id: null,
+                img: null,
+                title: 'Select Product',
+                placeholder: true,
+            });
+        });
+
+        if (itemSelected.length > 0) {
+            itemSelected.map((item, index) => {
+                selectProducts.splice(index, 1, item);
+            });
+            setSelected(selectProducts);
+        } else {
+            setSelected(selectProducts);
+        }
+        // }
+    }, [itemSelected, bundleSize]);
+
+    const variantSelected = selected.filter((sel) => sel.id !== null);
+
+    return (
+        <aside className={`lg:ml-3 ${isOpen ? 'before:content-[""] before:h-full before:w-full before:top-0 before:bottom-0 before:left-0 before:right-0 before:bg-black before:z-[9999] before:opacity-[.6] before:fixed lg:before:content-[none]' : ''}`}>
+            <div className={`lg:bg-primary-light lg:py-4 lg:px-2 lg:rounded-[2rem] ${isOpen ? 'fixed bottom-0 right-0 left-0 z-[9999] flex flex-col justify-end h-full' : 'mb-[1rem] lg:mb-0'} lg:static`}>
+                <div className={`flex bg-primary-light ${isOpen ? 'static rounded-t-[2rem] pt-4' : 'pt-2 fixed pb-[2rem]'} left-0 right-0 bottom-0 justify-between px-2 z-[1] lg:static lg:p-0`}>
+                    <p className="text-center lg:mb-[1rem] text-lg flex items-center lg:justify-center lg:w-full">
+                        <span className="font-bold lg:block">Your Bundle</span>
+                        <span className="ml-[.5rem] text-sm py-[.25rem] px-[.75rem] bg-white rounded-[.5rem] lg:hidden">{bundleDiscount}% OFF</span>
+                    </p>
+                    <div className="lg:hidden flex items-center text-primary font-bold" onClick={() => setIsOpen(!isOpen)}>
+                        <span className="mr-[.5rem] inline-block">View</span>
+                        <ChevronUp className={`w-g h-g svg--current-color ${isOpen ? 'rotate-180' : ''}`} />
+                    </div>
+                </div>
+                <div className="flex-wrap justify-center mb-[1rem] items-center hidden lg:flex">
+                    <span className="line-through text-lg mr-[.5rem] text-gray-600 opacity-[.5]">$163</span>
+                    <span className="text-lg mr-[.5rem] font-bold">$139</span>
+                    <span className="text-sm py-[.25rem] px-[.75rem] bg-white rounded-[.5rem]">{bundleDiscount}% OFF</span>
+                </div>
+                <div className={`${isOpen ? 'overflow-y-scroll lg:overflow-y-hidden' : 'hidden'} bg-primary-light px-2 pt-[1rem] lg:p-0 lg:block`}>
+                    <div className="flex mb-2">
+                        <PercentageSmall className="flex-[0_0_20px]" />
+                        <p className="ml-[.5rem] text-sm">If you add more products you hit more discount</p>
+                    </div>
+                    <ol>
+                        {selected.map((item, index) => (
+                            <ItemCard key={`sidebar--item-${index}`} item={item} placeholder={item.placeholder} store={store} index={index} />
+                        ))}
+                    </ol>
+                    <Button disabled={variantSelected.length !== bundleSize} buttonClass="mt-2 rounded-full border-primary bg-primary text-white w-full">{`${variantSelected.length}/${bundleSize} Selected`}</Button>
+                    <div className="flex mt-2">
+                        <DeliverySmall className="flex-[0_0_20px]" />
+                        <p className="ml-[.5rem] text-sm mb-2 lg:mb-0"><b>FREE</b> standard shipping on all orders above $50(SG)</p>
+                    </div>
+                    <p className="text-[#666] text-sm lg:hidden pb-4">*Cannot be combined with other discounts or promotions. If a Build Your Own Bundle is purchased, any extra discount or promo code will be removed.</p>
+                </div>
+            </div>
+            <p className="text-[#666] text-sm hidden lg:block mt-[1rem]">*Cannot be combined with other discounts or promotions. If a Build Your Own Bundle is purchased, any extra discount or promo code will be removed.</p>
+        </aside>            
+    );
+};
+
+export default YourBundleSidebar;
