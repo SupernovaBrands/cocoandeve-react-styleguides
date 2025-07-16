c5a0c213fc39301db7ba2e4d482f5262feb24cf1
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2025-07-16 11:05:30 +0700

    test video transcript

--- src/components/InstagramCard.tsx
+++ src/components/InstagramCard.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useCallback } from "react";
 import { useIsVisible } from "~/hooks/useIsVisible";
 
 const InstagramCard = (props: any) => {
-    const { videoUrl, author, title, url, classes, product } = props;
+    const { videoUrl, author, title, url, classes, product, index } = props;
 
     const { isVisible, targetRef } = useIsVisible(
         {
@@ -48,8 +48,9 @@ const InstagramCard = (props: any) => {
     return (
         <div className={`text-left rounded ${classes}`} ref={targetRef as any}>
             <video preload="none" ref={videoRef} 
-                className="lazy-video w-full mb-0 lazy-video bg-shimmer rounded-[1.5rem] h-[226px] min-h-[226px] lg:rounded-[2rem] lg:h-[355px] lg:min-h-[355px] object-cover" muted={true} playsInline={true} loop={true} autoPlay={false}>
+                className={`${index === 0 ? 'video--first' : ''} lazy-video w-full mb-0 lazy-video bg-shimmer rounded-[1.5rem] h-[226px] min-h-[226px] lg:rounded-[2rem] lg:h-[355px] lg:min-h-[355px] object-cover`} muted={true} playsInline={true} loop={true} autoPlay={false}>
                 <source src={videoUrl} type="video/mp4" />
+                {index === 0 && <track src="https://cdn.shopify.com/s/files/1/0286/1327/9779/files/captions.vtt?v=1752637151" kind="subtitles" srcLang="en" label="English" default />}
             </video>
             <p className="font-bold mb-25 mt-1 text-sm lg:text-base">{author}</p>
             <a href={`/products/${product}`} className="font-size-sm text-body block hover:text-body underline">{title}</a>
