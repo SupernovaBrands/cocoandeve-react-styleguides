771faeec42f0daba63ae9135f662e3c0595a1426
Teddy Maulana <maulanateddy99@gmail.com>
2025-09-04 11:02:57 +0700

    new update for free sample page

--- src/compounds/PageReviewCard.tsx
+++ src/compounds/PageReviewCard.tsx
@@ -16,10 +16,10 @@ const PageReviewCard = (props:any) => {
         }
     }, []);
     return (
-        <div key={props.keyName} className='bg-gray-400 rounded-[32px] review-comparision-card flex-col-reverse lg:flex-row relative mb-0 lg:mb-0 flex-grow-0 flex-shrink-0 flex lg:w-[100%] lg:basis-[100%] w-[85%] basis-[85%] pl-[16px] pr-[16px] pt-[16px] lg:px-g text-center'>
+        <div key={props.keyName} className='bg-gray-400 rounded-[32px] review-comparision-card flex-col-reverse lg:flex-row relative mb-0 lg:mb-0 flex-grow-0 flex-shrink-0 flex lg:w-[100%] lg:basis-[100%] w-[90%] basis-[90%] pl-[16px] pr-[16px] pt-[16px] lg:pt-0 lg:px-g text-center'>
             <div className='review-comparision-card__content w-[100%] lg:w-1/2 text-left align-items-center justify-center flex pt-[16px] lg:pt-[60px] pb-[16px] lg:pb-[60px]'>
-                <div className='flex flex-wrap pl-[16px] pr-[16px] lg:pl-[30px] lg:pr-[30px] max-w-[440px]'>
-                    <h3 className='mb-[16px] '>{data?.product_title}</h3>
+                <div className='flex flex-wrap pl-[0] pr-[0] lg:pl-[30px] lg:pr-[30px] max-w-[440px]'>
+                    <h3 className='mb-[4px] lg:mb-[16px] '>{data?.product_title}</h3>
                     <a href={`/products/${props?.productHandle}#write-a-review`} className="text-sm w-full block" aria-label="Write a review for this product">
                         <ReviewStar score="5" className={`flex`} />
                     </a>
@@ -30,7 +30,7 @@ const PageReviewCard = (props:any) => {
             <div className='review-comparision-card__image  w-[100%] lg:w-1/2'>
                 {comparisonImages && (
                     <div className="product-banner__image w-full h-full">
-                        <ProductBannerSlider {...comparisonImages} />
+                        <ProductBannerSlider {...comparisonImages} isPageReview={true} />
                     </div>
                 )}
             </div>
--- src/sections/SingleProductItem.tsx
+++ src/sections/SingleProductItem.tsx
@@ -2,7 +2,7 @@ const SingleProductItem = (props: any) => {
     const { data } = props;
     return (
         <>
-            <div className={`container px-0 text-center py-3 lg:pb-0 lg:px-0 mb-4 lg:pb-4`}>
+            <div className={`container px-0 text-center py-3 lg:pb-0 lg:px-0 mb-0 lg:pb-4`}>
                 <h2 className="text-xl lg:text-2xl text-center mb-g lg:mb-1 mx-5 lg:mx-0 ">{'Always wanted to try Coco & Eve?'}</h2>
                 <p className='mb-g mx-4 lg:mx-0 lg:mb-3'>{'Enjoy FREE mini version of some of our bestsellers—just pay shipping.'}</p>
 
@@ -17,17 +17,19 @@ const SingleProductItem = (props: any) => {
                                 className="object-cover h-full w-full" loading="lazy" height="357" width="414" alt={"Product banner and comparison image"} />
                         </picture>
                     </div>
-                    <div className="single-product-item__right text-left w-full lg:w-1/2 relative max-w-[315px] mt-[-60px] mb-0 mx-auto p-4 rounded-[32px] bg-white p-[16px] lg:mr-[-90px] lg:max-w-[570px] lg:ml-0 lg:my-[30px] lg:p-[30px]">
-                        <h2 className="h4 lg:text-xl lg:font-bold mb-[8px] block">{data?.product_title || ''}</h2>
-                        <p className="text-sm mb-[8px] lg:mb-[16px]">{data?.description}</p>
-                        <ul className="text-sm  list-check ">
-                            {data?.list?.split('\n').map((li: any) => {
-                                return (
-                                    <li className="lg:mb-[8px]">{li}</li>
-                                )
-                            })}``
-                        </ul>
-                        <a href={data?.button_url} className="btn btn-large btn-primary mt-[24px] rounded-[32px] w-full p-g">{data?.button_label || ''}</a>
+                    <div className="single-product-item__right text-left w-full lg:w-1/2 relative   mb-0 mx-auto   flex justify-center lg:block">
+                        <div className="rounded-[32px] bg-white  p-[16px] pt-[20px] lg:my-[30px] lg:p-[30px] max-w-[315px] lg:max-w-[570px] mt-[-85px] lg:mr-[-90px] lg:ml-auto">
+                            <div className="flex mb-[8px] items-center"><h2 className="h4 lg:text-xl lg:font-bold mb-0">{data?.product_title || ''}</h2> <span className="block mx-1 text-sm">•</span> <span className="text-sm">5ml</span></div>
+                            <p className="text-sm mb-[8px] lg:mb-[16px] lg:max-w-[410px]">{data?.description}</p>
+                            <ul className="text-sm  list-check page-product-list ">
+                                {data?.list?.split('\n').map((li: any) => {
+                                    return (
+                                        <li className="lg:!mb-[10px]">{li}</li>
+                                    )
+                                })}
+                            </ul>
+                            <a href={data?.button_url} className="btn btn-large btn-primary mt-[20px] rounded-[32px] w-full p-g lg:max-w-[236px] font-normal">{data?.button_label || ''}</a>
+                        </div>
                     </div>
                 </div>
             </div>
