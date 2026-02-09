493866fd5917ab63ebcc03abdb474efa926721f2
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2026-02-09 19:59:33 +0700

    [staging] fix stars error

--- src/components/YotpoStars.tsx
+++ src/components/YotpoStars.tsx
@@ -73,7 +73,7 @@ const YotpoStar = (props: any) => {
 	}, [isVisible, isVisible, init, props.sku]);
 
 	return init ? (
-		<div ref={containerRef} className={`flex items-center ${props.className}`} data-skus={props.sku}>
+		<div className={`flex items-center ${props.className}`} data-skus={props.sku}>
 			<ConditionalWrap
                 condition={props?.productHandle}
                 wrap={children => <a href={`/products/${props?.productHandle}?write-a-review=true`} className="text-sm" aria-label="Write a review for this product">{children}</a>}
@@ -90,35 +90,72 @@ const YotpoStar = (props: any) => {
 			</a> */}
 			{props.smSingleStar && (
 				<>
-					<a href={`/products/${props?.productHandle}?write-a-review=true`} className="text-sm" aria-label="Write a review for this product">
-						{!props.smSingleStarAllDevice && (
-							<ReviewStar score={score} className={`${props.smSingleStar ? 'review-star__v1 hidden lg:flex' : 'flex'}`} />
-						)}
+					<a className="review-star__v2" href={`/products/${props?.productHandle}?write-a-review=true`} aria-label="Write a review for this product">
+						<ReviewStarSingle className={`${props.smSingleStarAllDevice ? '' : 'lg:hidden'}`} />
 					</a>
-					{props.smSingleStar && (
-						<>
-							<a className="review-star__v2" href={`/products/${props?.productHandle}?write-a-review=true`} aria-label="Write a review for this product">
-								<ReviewStarSingle className={`${props.smSingleStarAllDevice ? '' : 'lg:hidden'}`} />
-							</a>
-							<a className="review-star__v2" href={`/products/${props?.productHandle}?write-a-review=true`} aria-label="Write a review for this product">
-								<span className={`${props.smSingleStarAllDevice ? '' : 'lg:hidden'} ml-25`}>{score ? score.toFixed(1) : 0}/5.0</span>
-							</a>
-						</>
-					)}
-					{props.showScore && score && (
-						<span className="ml-25">({score?.toFixed(0)})</span>
-					)}
-					{props.showTotal && (
-						<span className="ml-25 review-star__total">
-							<a href={`/products/${props?.productHandle}?write-a-review=true`} className={`${props.smSingleStar || props.sustainability ? '' : ''} text-xs text-body hover:text-primary ${props.hideUnderline ? '' : 'underline'} yotpo-start__number`} aria-label={`Total reviews (${total?.toFixed(0)})`}>({total?.toFixed(0)})</a>
-						</span>
-					)}
+					<a className="review-star__v2" href={`/products/${props?.productHandle}?write-a-review=true`} aria-label="Write a review for this product">
+						<span className={`${props.smSingleStarAllDevice ? '' : 'lg:hidden'} ml-25`}>{`${score ? score.toFixed(1) : 0}/5.0`}</span>
+					</a>
 				</>
+			)}
+			{props.showScore && score && <span className="ml-25">({`${score?.toFixed(0)}`})</span>}
+			{props.showTotal && (
+				<span className="ml-25 review-star__total">
+					<a href={`/products/${props?.productHandle}?write-a-review=true`} className={`${props.smSingleStar || props.sustainability ? '' : ''} text-xs text-body hover:text-primary ${props.hideUnderline ? '' : 'underline'} yotpo-start__number`} aria-label={`Total reviews (${total?.toFixed(0)})`}>({total?.toFixed(0)})</a>
+				</span>
 			)}
 		</div>
 	) : (
 		<div ref={containerRef} className="h-2 w-full" aria-hidden="true" />
 	);
+
+	// return init ? (
+	// 	<div ref={containerRef} className={`flex items-center ${props.className}`} data-skus={props.sku}>
+	// 		<ConditionalWrap
+    //             condition={props?.productHandle}
+    //             wrap={children => <a href={`/products/${props?.productHandle}?write-a-review=true`} className="text-sm" aria-label="Write a review for this product">{children}</a>}
+    //             elseWrap={children => <div className="text-sm">{children}</div>}
+    //         >
+	// 			{!props.smSingleStarAllDevice && (
+	// 				<ReviewStar score={score} className={`${props.smSingleStar ? 'review-star__v1 hidden lg:flex' : 'flex'}`} />
+	// 			)}
+	// 		</ConditionalWrap>
+	// 		{/* <a href={`/products/${props?.productHandle}#write-a-review`} className="text-sm" aria-label="Write a review for this product">
+	// 			{!props.smSingleStarAllDevice && (
+	// 				<ReviewStar score={score} className={`${props.smSingleStar ? 'review-star__v1 hidden lg:flex' : 'flex'}`} />
+	// 			)}
+	// 		</a> */}
+	// 		{props.smSingleStar && (
+	// 			<>
+	// 				<a href={`/products/${props?.productHandle}?write-a-review=true`} className="text-sm" aria-label="Write a review for this product">
+	// 					{!props.smSingleStarAllDevice && (
+	// 						<ReviewStar score={score} className={`${props.smSingleStar ? 'review-star__v1 hidden lg:flex' : 'flex'}`} />
+	// 					)}
+	// 				</a>
+	// 				{props.smSingleStar && (
+	// 					<>
+	// 						<a className="review-star__v2" href={`/products/${props?.productHandle}?write-a-review=true`} aria-label="Write a review for this product">
+	// 							<ReviewStarSingle className={`${props.smSingleStarAllDevice ? '' : 'lg:hidden'}`} />
+	// 						</a>
+	// 						<a className="review-star__v2" href={`/products/${props?.productHandle}?write-a-review=true`} aria-label="Write a review for this product">
+	// 							<span className={`${props.smSingleStarAllDevice ? '' : 'lg:hidden'} ml-25`}>{score ? score.toFixed(1) : 0}/5.0</span>
+	// 						</a>
+	// 					</>
+	// 				)}
+	// 			</>
+	// 		)}
+	// 		{props.showScore && score && (
+	// 					<span className="ml-25">({score?.toFixed(0)})</span>
+	// 				)}
+	// 				{props.showTotal && (
+	// 					<span className="ml-25 review-star__total">
+	// 						<a href={`/products/${props?.productHandle}?write-a-review=true`} className={`${props.smSingleStar || props.sustainability ? '' : ''} text-xs text-body hover:text-primary ${props.hideUnderline ? '' : 'underline'} yotpo-start__number`} aria-label={`Total reviews (${total?.toFixed(0)})`}>({total?.toFixed(0)})</a>
+	// 					</span>
+	// 				)}
+	// 	</div>
+	// ) : (
+	// 	<div ref={containerRef} className="h-2 w-full" aria-hidden="true" />
+	// );
 };
 
 YotpoStar.defaultProps = {
