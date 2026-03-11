e0d27e2e3a4beead4418f16d4024a06305e7587e
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2026-03-11 08:59:48 +0700

    [86ew421qj] remove log

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -53,7 +53,7 @@ const Cart: React.FC<Props> = (props) => {
 		onUpdateCart, onDeleteLine, discountMeter, shippingMeter,
 		removeDiscount, shippingData, handleDiscount, manualGwpSetting, changeVariant, trackEvent, tiktokEvent, fbqEvent, currency, user, isAuthenticated, strapiCartSetting, cartUpsell, addToCart } = props;
 	// const storeApi = new storefrontApi();
-	console.log(manualGwpSetting, 'manualGwpSetting');
+	// console.log(manualGwpSetting, 'manualGwpSetting');
 	// const [loadingInit, setLoadingInit] = useState(props.isLoading);
 	const [cart, setCart] = useState({
 		id: '', items: [], lines: { edges: [] }, discountAllocations: [], discountCodes: [], buyerIdentity: {},
