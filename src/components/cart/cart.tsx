3d0b0c17dd3c4b13008d601c531033f85f8a95d1
Teddy Maulana <maulanateddy99@gmail.com>
2026-04-23 10:56:34 +0700

    update spaces

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -394,7 +394,7 @@ const Cart: React.FC<Props> = (props) => {
 									cart={cartData}
 								/>
 
-								<div className="flex flex-wrap mt-3 mb-3 lg:mt-[30px] lg:mb-1">
+								<div className="flex flex-wrap mt-3 mb-1 lg:mt-[30px] lg:mb-1">
 									<p className="w-2/3 mb-[5px] font-bold" data-cy="cart-subtotal-label">{tStrings.cart_subtotal}</p>
 									<p className="w-1/3 mb-[5px] font-bold text-right" data-cy="cart-subtotal-value">{formatMoney(cart.subtotalPrice, false, store)}</p>
 
